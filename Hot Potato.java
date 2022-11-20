60202fa15869755d18968ca9f4426721588b4178
Deyan <deyanm@gmail.com>
2022-11-20 17:29:37 +0200

    empty

---
+++ Hot Potato.java
@@ -0,0 +1,30 @@
+package DPM;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        String line ;
+
+        while (!"print".equalsIgnoreCase(line= scan.nextLine())){
+
+            if (!line.equals("cancel")){
+                queue.offer(line);
+
+
+            }else {
+                if (queue.size() !=0){
+                    System.out.println("Canceled " + queue.poll());
+                }else {
+                    System.out.println("Printer is on standby");
+                }
+            }
+        }
+        while (queue.size() != 0){
+            System.out.println(queue.poll());
+        }
+    }
+}
