ce1e0c9ba11cc969f7122de653cdf9d28eac07bc
Deyan <deyanm@gmail.com>
2022-12-15 19:15:18 +0200

    empty

---
+++ Knights of Honor.java
@@ -0,0 +1,14 @@
+package DPM;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split("\\s+");
+        Consumer<String > print = word -> System.out.println("Sir " +word);
+        Arrays.stream(line).forEach(print);
+    }
+}
