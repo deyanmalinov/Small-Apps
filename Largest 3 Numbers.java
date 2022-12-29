b5cc1859e85edfabf8b2855824d8e7e555343f9c
Deyan <deyanm@gmail.com>
2022-12-29 19:11:04 +0200

    empty

---
+++ Largest 3 Numbers.java
@@ -0,0 +1,16 @@
+package DPM;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println(Arrays.stream(scan.nextLine().split(" "))
+                    .map(n -> Integer.parseInt(n))
+                    .sorted(Collections.reverseOrder())
+                    .limit(3)
+                    .map(n -> String.valueOf(n))
+                    .collect(Collectors.joining(" ")));
+        }
+}
