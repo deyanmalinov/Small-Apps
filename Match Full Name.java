227c6f43e510ae8a4b1608925e588b2f99840ad6
Deyan <deyanm@gmail.com>
2023-01-13 21:37:31 +0200

    empty

---
+++ Match Full Name.java
@@ -0,0 +1,22 @@
+package DPM;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+            String string = scan.nextLine();
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(string);
+
+            while (matcher.find()) {
+                System.out.print(matcher.group(0)+" ");
+
+            }
+
+        }
+}
