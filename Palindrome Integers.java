a5256099ec2972e9d61be9f53c958f42e5f35081
Deyan <deyanm@gmail.com>
2023-01-16 00:32:18 +0200

    empty

---
+++ Palindrome Integers.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.*;
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String line = "";
+            while (true){
+
+                line = scan.nextLine();
+                if (line.equals("END")){
+                    break;
+                }
+                for (int i = 0; i <= line.length()/2; i++) {
+                    int lenth = line.length();
+
+                    if (line.charAt(i) == line.charAt(lenth-1)){
+                        lenth--;
+                        System.out.println(true);
+                        break;
+                    }else {
+                        System.out.println(false);
+                        break;
+                    }
+                }
+
+            }
+    }
+}
