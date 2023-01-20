c0724bcf3a3fd4a5965a4f157cf82699ec9cf70f
Deyan <deyanm@gmail.com>
2023-01-20 10:20:07 +0200

    empty

---
+++ Parking Lot.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        HashSet<String> parking = new HashSet<>();
+        String line;
+        while (!"END".equals(line=scan.nextLine())){
+            String[] nums = line.split(", ");
+            switch (nums[0]){
+                case "IN":
+                    parking.add(nums[1]);
+                    break;
+                case "OUT":
+                    parking.remove(nums[1]);
+            }
+        }
+        if (parking.isEmpty()){
+            System.out.println("Parking Lot is Empty");
+        }else {
+            for (String s : parking) {
+                System.out.println(s);
+            }
+        }
+    }
+}
