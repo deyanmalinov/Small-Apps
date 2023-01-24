b4218cbddfdcb24acb5221ca4330609b89b3bbca
Deyan <deyanm@gmail.com>
2023-01-24 10:43:07 +0200

    empty

---
+++ Orders.java
@@ -0,0 +1,31 @@
+package com.DPM;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            Map<String, Double> fmap = new LinkedHashMap<>();
+            Map<String, Integer> smap = new LinkedHashMap<>();
+            String line = "";
+            while (!"buy".equals(line = scan.nextLine())){
+                String[] comand = line.split(" ");
+                String product = comand[0];
+                double price  = Double.parseDouble(comand[1]);
+                int amaunt = Integer.parseInt(comand[2]);
+
+                if (!fmap.containsKey(product)){
+                    fmap.put(product, amaunt*price);
+                    smap.put(product, amaunt);
+                }else {
+
+                    smap.put(product, smap.get(product)+amaunt);
+                    fmap.put(product, smap.get(product)*price);
+                }
+            }
+           fmap.forEach((key, value) -> System.out.println(String.format("%s -> %.2f", key, value)));
+
+        }
+}
