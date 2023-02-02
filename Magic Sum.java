7050c0799917820b5dc35da46c8959381e995a79
Deyan <deyanm@gmail.com>
2023-02-02 16:49:37 +0200

    empty

---
+++ Magic Sum.java
@@ -0,0 +1,22 @@
+package DPM;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] line = scan.nextLine().split(" ");
+        int [] nums = new int[line.length];
+        int sum = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(line[i]);
+        }
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int num = nums[i] + nums[j];
+                if (sum == num){
+                    System.out.println(nums[i]+" "+ nums[j]);
+                }
+            }
+        }
+
+    }
+}
