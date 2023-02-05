9ea8603ab1ff827a082f68e0cdf533bbd42623a4
Deyan <deyanm@gmail.com>
2023-02-05 23:52:29 +0200

    empty

---
+++ Matching Brackets.java
@@ -0,0 +1,25 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < line.length(); i++) {
+            char ch = line.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            }
+            else if (ch == ')') {
+                int startIndex = stack.pop();
+                String contents =
+                        line.substring(startIndex, i + 1);
+                System.out.println(contents);
+            }
+
+        }
+        }
+}
