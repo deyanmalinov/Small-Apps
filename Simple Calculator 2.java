e6d28fd5f231f8c0f1357328bbea6682d14bc69a
Deyan <deyanm@gmail.com>
2023-02-21 18:52:44 +0200

    empty

---
+++ Simple Calculator 2.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //2 + 5 + 10 - 2 - 1
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split(" ");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        //Collections.addAll(stack, line);
+        for (int i = line.length-1; i > -1; i--) {
+            stack.push(line[i]);
+        }
+       while (stack.size()>1){
+           int a = Integer.parseInt(stack.pop());
+           String znak = stack.pop();
+           int b = Integer.parseInt(stack.pop());
+           if (znak.equals("+")){
+               stack.push(""+(a+b));
+           }else {
+               stack.push(""+(a-b));
+           }
+         }
+        for (String s : stack) {
+            System.out.println(s);
+        }
+    }
+}
