d25e9e535c8fe1928058ca818478e239964263ff
Deyan <deyanm@gmail.com>
2023-02-21 15:41:26 +0200

    empty

---
+++ Simple Calculator 1.java
@@ -0,0 +1,28 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split(" ");
+        Deque<String> stack = new ArrayDeque<>();
+        Collections.addAll(stack, line);
+
+        while (stack.size() > 1){
+            int first = Integer.parseInt(stack.pop());
+            String sign = stack.pop();
+            int second = Integer.parseInt(stack.pop());
+
+            switch (sign){
+                case "+":
+                    stack.push(String.valueOf(first + second));
+                    break;
+                case "-":
+                    stack.push(String.valueOf(first-second));
+                    break;
+            }
+        }
+        System.out.println(stack.pop());
+        }
+}
