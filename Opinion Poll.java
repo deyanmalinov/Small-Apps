161a9fbf92c6caf86c8efb166e428c1446e6336a
Deyan <deyanm@gmail.com>
2023-02-27 08:46:23 +0200

    empty

---
+++ Opinion Poll.java
@@ -0,0 +1,45 @@
+package DPM;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lines = Integer.parseInt(scan.nextLine());
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i <lines; i++) {
+            String[] line = scan.nextLine().split(" ");
+            String name = line[0];
+            int age = Integer.parseInt(line[1]);
+            Person persona = new Person(name, age);
+            people.add(persona);
+        }
+        people.stream()
+                .filter(person -> person.getAge()>30)
+                .sorted((fp, sp) -> fp.getName().compareTo(sp.getName()))
+                .forEach(person -> System.out.println(String
+                .format("%s - %d",person.getName(),person.getAge())));
+    }
+}
+-------------------------------------------------------------------
+package DPM;
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
