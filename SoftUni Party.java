be9ca2c9c5023e0a280531d551e1e6c3d01d9a66
Deyan <deyanm@gmail.com>
2023-03-01 14:08:44 +0200

    empty

---
+++ SoftUni Party.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TreeSet<String> guest = new TreeSet<>();
+        String line;
+        while (!"PARTY".equals(line = scan.nextLine())){
+            guest.add(line);
+        }
+        while (!"END".equals(line = scan.nextLine())){
+            guest.remove(line);
+        }
+        System.out.println(guest.size());
+        for (String s : guest) {
+            System.out.println(s);
+        }
+    }
+}
