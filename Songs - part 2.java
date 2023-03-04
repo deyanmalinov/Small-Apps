c3f303b9a02d991db2c6cb14badf5695f8ba9d5a
Deyan <deyanm@gmail.com>
2023-03-04 17:20:49 +0200

    empty

---
+++ Songs - part 2.java
@@ -0,0 +1,27 @@
+package DPM;
+
+public class Songs {
+    private String typeList;
+    private String name;
+    private String time;
+
+    public String getTypeList() {
+        return typeList;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
