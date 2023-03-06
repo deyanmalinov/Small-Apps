2dc4f571eb836f6cabb0ad37efa462200546bd46
Deyan <deyanm@gmail.com>
2023-03-06 21:52:59 +0200

    empty

---
+++ Students.java
@@ -0,0 +1,47 @@
+package DPM;
+
+public class Student {
+    private String fname;
+    private String sname;
+    private String age;
+    private String town;
+
+    public Student(String fname, String sname, String age, String town) {
+        this.fname = fname;
+        this.sname = sname;
+        this.age = age;
+        this.town = town;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getTown() {
+        return town;
+    }
+}
