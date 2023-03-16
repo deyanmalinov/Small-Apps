d6e120417ed2c7ed5e278daf904cb995377cdcb3
Deyan <deyanm@gmail.com>
2023-03-16 18:50:46 +0200

    empty

---
+++ StringArr to IntegerArr with Function.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split(" ");
+        Function<String, Integer> parse = str -> Integer.parseInt(str);
+        Integer[] newArr = new Integer[line.length];
+        for (int i = 0; i < line.length; i++) {
+            newArr[i] = parse.apply(line[i]);
+        }
+        for (Integer integer : newArr) {
+            System.out.print(integer+" ");
+        }
+    }
+}
