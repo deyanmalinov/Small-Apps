027edd4f4dfd19c6a5b4789b52bf05b065085d4a
Deyan <deyanm@gmail.com>
2023-03-22 14:33:33 +0200

    empty

---
+++ Sum Digits.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String num = scan.nextLine();
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            sum += num.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+
+}
