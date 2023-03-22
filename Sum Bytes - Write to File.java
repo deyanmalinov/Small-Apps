b9f72ec41190a66bac3db2bcb383366cb0daa8bb
Deyan <deyanm@gmail.com>
2023-03-22 18:34:39 +0200

    empty

---
+++ Sum Bytes - Write to File.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path = "D:\\Coding\\BG_Softuni\\Java Advanced - January 2019" +
+                "\\04.1. Java-Advanced-Files-and-Directories-Exercises\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        String output = "D:\\Coding\\BG_Softuni\\Java Advanced - January 2019" +
+                "\\04.1. Java-Advanced-Files-and-Directories-Exercises\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        PrintWriter writer = new PrintWriter(new FileWriter(output));
+        try {
+            String line = reader.readLine();
+            int sum = 0;
+            while (line != null){
+                for (char chars : line.toCharArray()) {
+                    sum+=chars;
+                }
+                line=reader.readLine();
+            }
+            writer.write(sum+"");
+            System.out.println(sum);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer.close();
+    }
+}
