41cd8558a302aefe597a2da901419891799ddb8b
Deyan <deyanm@gmail.com>
2023-03-26 19:43:58 +0300

    empty

---
+++ Substring.java
@@ -0,0 +1,16 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String sub = scan.nextLine();
+        String line = scan.nextLine();
+
+        while (line.contains(sub)){
+             line= line.replace(sub, "");
+        }
+        System.out.println(line);
+    }
+}
