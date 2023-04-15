fc392605954cbd5911e98cc73d9192db1c5c67bf
Deyan <deyanm@gmail.com>
2023-04-15 21:06:57 +0300

    empty

---
+++ Sum of Chars.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int lines = Integer.parseInt(scan.nextLine());
+            int sum = 0;
+            for (int i = 0; i < lines; i++) {
+                String chark = scan.nextLine();
+                char znak = chark.charAt(0);
+                int znakASCI = znak;
+                sum +=znakASCI;
+
+            }
+            System.out.println("The sum equals: " + sum);
+        }
+
+}
