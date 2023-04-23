2e5fae8e09473b677fb1a06106b0575614512e1b
Deyan <deyanm@gmail.com>
2023-04-23 20:34:39 +0300

    empty

---
+++ Replace Repeating Chars.java
@@ -0,0 +1,21 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String res ="";
+        char ch = text.charAt(0);
+        res += ch;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (ch != symbol){
+                ch=symbol;
+                res +=ch;
+            }
+        }
+        System.out.println(res);
+    }
+}
