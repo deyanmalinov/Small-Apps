3bcba1a35e181184930349ea3059187259caa438
Deyan <deyanm@gmail.com>
2023-04-23 21:26:46 +0300

    empty

---
+++ Repeat strings.java
@@ -0,0 +1,17 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] line = scan.nextLine().split(" ");
+        for (int i = 0; i < line.length; i++) {
+            String ele = line[i];
+            int lenght = ele.length();
+            for (int j = 0; j < lenght; j++) {
+                System.out.print(ele);
+            }
+        }
+    }
+}
