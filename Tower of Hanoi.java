f221b07e227a3c44b7ac24ce14464fa746c94703
Deyan <deyanm@gmail.com>
2023-04-26 16:44:00 +0300

    empty

---
+++ Tower of Hanoi.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.ArrayDeque;
+public class Main {
+    public static void main(String[] args){
+        ArrayDeque<Integer> first = new ArrayDeque<>();
+        first.push(3);
+        first.push(2);
+        first.push(1);
+        ArrayDeque<Integer> spare = new ArrayDeque<>();
+        ArrayDeque<Integer> last = new ArrayDeque<>();
+        int disk = 3;
+        Move(3, first, spare, last);
+        for (Integer integer : last) {
+            System.out.println(integer);
+        }
+    }
+    private static void Move(int disk, ArrayDeque<Integer> first, ArrayDeque<Integer> spare, ArrayDeque<Integer> last) {
+        if (disk < 1) {
+            return;
+        }else {
+
+            Move(disk -1, first,last,spare);
+            last.push(disk);
+            first.pop();
+            Move(disk -1, spare,first,last);
+        }
+    }
+}
