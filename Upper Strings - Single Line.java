1fdc3ef18f71db7c8e275c58186d2aa064e57d8b
Deyan <deyanm@gmail.com>
2023-05-11 10:01:16 +0300

    empty

---
+++ Upper Strings - Single Line.java
@@ -0,0 +1,27 @@
+package DPM;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<String> names = Arrays.stream(scan.nextLine()
+                .split(" "))
+                .collect(Collectors.toList());
+
+        names.stream()
+                .map(name -> name.toUpperCase())
+                .forEach(name -> System.out.print(name + " "));
+//////////////////////////////////////////////////////////////
+Arrays.stream(scan.nextLine()
+                .split(" "))
+                .collect(Collectors.toList()).stream()
+                .map(name -> name.toUpperCase())
+                .forEach(name -> System.out.print(name + " "));
+/////////////////////////////////////////////////////////////
+
+    }
+}
