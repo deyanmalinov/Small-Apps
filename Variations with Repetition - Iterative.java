4bd703629813d6fc1c667032539d7102c8f901f7
Deyan <deyanm@gmail.com>
2023-05-14 19:00:06 +0300

    empty

---
+++ Variations with Repetition - Iterative.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arrSize = 3;
+        int num = 5;
+        int[] arr = new int[arrSize];
+
+        while (true){
+            Print(arr);
+            int index = arrSize-1;
+            while (index >= 0 && arr[index] == 4){
+                index--;
+            }
+            if (index < 0){
+                break;
+            }
+            arr[index]++;
+            for (int i = index+1; i < arrSize ; i++) {
+                arr[i]=0;
+            }
+        }
+    }
+    private static void Print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
