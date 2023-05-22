1ee14eb1abe33e72ece3a39cd2f5a3ff08ddf464
Deyan <deyanm@gmail.com>
2023-05-22 12:03:02 +0300

    empty

---
+++ Print Diagonals of Square Matrix.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = Integer.parseInt(scan.nextLine());
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            int[] line = Arrays.stream(scan.nextLine().split(" ")).
+                    mapToInt(Integer::parseInt).toArray();
+            matrix[i] = line;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(matrix[i][i]+" ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print(matrix[size-1-i][i]+" ");
+        }
+        System.out.println();
+    }
+}
