8baeec2b4276e95d9608ed7113595dc6f06314cb
Deyan <deyanm@gmail.com>
2023-05-24 12:05:37 +0300

    empty

---
+++ Positions Of.java
@@ -0,0 +1,34 @@
+package DPM;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[] matSize = scan.nextLine().split(" ");
+        int rows = Integer.parseInt(matSize[0]);
+        int cols = Integer.parseInt(matSize[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < matrix.length; row++) {
+            String[] line = scan.nextLine().split(" ");
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[row][col] = Integer.parseInt(line[col]);
+            }
+        }
+        int num = scan.nextInt();
+        boolean flag = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == num){
+                    System.out.println(i + " "+ j);
+                    flag = true;
+                }
+
+            }
+
+        }if (!flag){
+            System.out.print("not found");
+        }
+
+    }
+}
