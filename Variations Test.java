e7dcdc510d608a7e488ad2bb832ecf825a6509b2
Deyan <deyanm@gmail.com>
2023-05-26 23:23:58 +0300

    empty

---
+++ Variations Test.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        fillArray(arr, 0);
+    }
+     private static void fillArray(int[] arr, int index) {
+         if (index >= arr.length) {
+             printArr(arr);
+             return;
+         }
+             for (int i = 0; i <= 1; i++) {
+                 arr[index] = i;
+                 fillArray(arr, index + 1);
+             }
+         }
+     private static void printArr(int[] arr) {
+         for (int i : arr) {
+             System.out.print(i);
+         }
+        System.out.println();
+    }
+}
