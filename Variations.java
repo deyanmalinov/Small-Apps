e457e65ebfe5636c1a50650c4fd793e4fbffab8d
Deyan <deyanm@gmail.com>
2023-05-30 10:51:05 +0300

    empty

---
+++ Variations.java
@@ -0,0 +1,35 @@
+package com.DPM;
+
+public class Variations {
+        static int a = 3;
+        static int b = 4;
+        static int[] mainArr = new int[a];
+        static int[] tempArr = new int[]{1, 2, 3, 4};
+        public static void main(String[] args) {
+            Gen(0);
+            Swap(a, b);
+        }
+        private static void Gen(int index) {
+            if (index >= a){
+                Print();
+            }else {
+                for (int i = index; i < b; i++) {
+                    mainArr[index] = tempArr[i];
+                    Swap(tempArr[i], tempArr[index]);
+                    Gen(index + 1);
+                    Swap(tempArr[i], tempArr[index]);
+                }
+            }
+        }
+        static void Swap(int ind1, int ind2){
+            int temp = ind1;
+            ind1 = ind2;
+            ind2 = temp;
+        }
+        static void Print(){
+            for (int i : mainArr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+    }
+}
---
+++ VoinaNumbersGame.java
@@ -0,0 +1,47 @@
+package com.DPM;
+
+import java.util.*;
+
+public class VoinaNumbersGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Set<Integer> firstPlayer = getCollect(scan.nextLine());
+        Set<Integer> secondPlayer = getCollect(scan.nextLine());
+
+        for (int i = 0; i < 50; i++) {
+            if (firstPlayer.isEmpty() || secondPlayer.isEmpty()) break;
+
+            Iterator<Integer> fPlaIter = firstPlayer.iterator();
+            Iterator<Integer> sPlaIter = secondPlayer.iterator();
+
+            int firPVal = fPlaIter.next();
+            firstPlayer.remove(firPVal);
+            int secPVal = sPlaIter.next();
+            secondPlayer.remove(secPVal);
+
+            if (firPVal > secPVal){
+                firstPlayer.add(firPVal);
+                firstPlayer.add(secPVal);
+            }else if (secPVal > firPVal){
+                secondPlayer.add(firPVal);
+                secondPlayer.add(secPVal);
+            }else {
+                firstPlayer.add(firPVal);
+                secondPlayer.add(secPVal);
+            }
+        }
+        if (firstPlayer.size() > secondPlayer.size()){
+            System.out.println("First player win!");
+        }else if (secondPlayer.size() > firstPlayer.size()){
+            System.out.println("Second player win!");
+        }else {
+            System.out.println("Draw");
+        }
+    }
+
+    static LinkedHashSet<Integer> getCollect(String line) {
+        LinkedHashSet<Integer> res = new LinkedHashSet<>();
+        Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).forEach(res::add);
+        return res;
+    }
+}
