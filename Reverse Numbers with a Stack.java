5ea98aefa1eac098d730bdac47becb6ea9c58586
Deyan <deyanm@gmail.com>
2023-06-01 12:28:49 +0300

    empty

---
+++ Reverse Numbers with a Stack.java
@@ -0,0 +1,16 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] line = scan.nextLine().split(" ");
+        ArrayDeque<Integer> nums = new ArrayDeque<>();
+        for (int i = 0; i < line.length; i++) {
+            nums.push(Integer.parseInt(line[i]));
+        }
+        for (Integer num : nums) {
+            System.out.print(nums.pop()+" ");
+        }
+    }
+}
