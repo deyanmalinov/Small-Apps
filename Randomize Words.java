d2b9aa13c97913a907d54f31aedcf17700b2015e
Deyan <deyanm@gmail.com>
2023-07-02 20:30:35 +0300

    empty

---
+++ Randomize Words.java
@@ -0,0 +1,22 @@
+package DPM;
+import java.util.*;
+import java.util.stream.Collectors;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String [] line = scan.nextLine().split(" ");
+        List<String> fpD = new ArrayList<>();
+        for (int i = 0; i < line.length; i++) {
+            fpD.add(i, line[i]);
+        }
+//        Collections.shuffle(fpD);
+//        System.out.println(String.join(System.lineSeparator(), fpD));
+
+        Random rnd = new Random();
+        while (!fpD.isEmpty()){
+            int num = rnd.nextInt(fpD.size());
+            System.out.println(fpD.get(num));
+            fpD.remove(num);
+        }
+    }
+}
