07f4afed21bf2d4eda2042df123811fd0f01ba54
Deyan <deyanm@gmail.com>
2023-07-05 14:31:47 +0300

    empty

---
+++ Spice Must Flow.java
@@ -0,0 +1,21 @@
+package DPM;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int spice = scan.nextInt();
+        int days=0;
+        int tatalSpice = 0;
+        while (spice>=100){
+            days++;
+            tatalSpice += (spice-26);
+            spice -=10;
+        }
+        if (tatalSpice>=26){
+            tatalSpice -=26;
+        }
+        System.out.println(days);
+        System.out.println(tatalSpice);
+
+    }
+}
