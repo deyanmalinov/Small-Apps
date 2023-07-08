13e57d11ad9ac3a00cef12f073fe48f6036d8e47
Deyan <deyanm@gmail.com>
2023-07-08 21:57:22 +0300

    empty

---
+++ String Explosion.java
@@ -0,0 +1,35 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       String text = scan.nextLine();
+       String res = "";
+        for (int i = 0; i < text.length(); i++) {
+            char bomb = text.charAt(i);
+            if (bomb =='>'){
+                res+=bomb;
+                i++;
+                int explosion = text.charAt(i) - '0';
+                explosion--;
+                while (explosion > 0 && i < text.length()-1){
+                    i++;
+                    bomb = text.charAt(i);
+                    if (bomb == '>'){
+                        res+=bomb;
+                        i++;
+                        explosion += text.charAt(i) - '0';
+                        explosion--;
+                        continue;
+                    }
+                   explosion--;
+                }
+            }else {
+                res+= bomb;
+            }
+        }
+        System.out.println(res);
+    }
+}
