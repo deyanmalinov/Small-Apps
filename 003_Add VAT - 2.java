6394b88fd96ce9e8417a810af3fabbe3d94685c2
Deyan Malinov <71469670+deyanmalinov@users.noreply.github.com>
2023-07-13 19:10:28 +0300

    Add files via upload

---
+++ 003_Add VAT - 2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split(", ");
+        UnaryOperator<Double> addVat = d -> 1.2 * d;
+        System.out.println("Prices with VAT:");
+        Arrays.stream(line).map(Double::parseDouble)
+                .map(addVat)
+                .forEach(num -> System.out.printf("%.2f\n", num));
+    }
+}
