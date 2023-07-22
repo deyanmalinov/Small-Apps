e7d2c2efeb5bb57e452d65d1213c22db683039ac
Deyan Malinov <71469670+deyanmalinov@users.noreply.github.com>
2023-07-22 22:27:06 +0300

    Add files via upload

---
+++ 017_Beer Kegs.java
@@ -0,0 +1,25 @@
+package DPM;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int beerNum = Integer.parseInt(scan.nextLine());
+        String beerName, bearNameBig="";
+        double rad, volume, bigVolume=0;
+        int hig;
+        while (beerNum>0){
+            beerName = scan.nextLine();
+            rad = Double.parseDouble(scan.nextLine());
+            hig = Integer.parseInt(scan.nextLine());
+            volume = Math.PI * rad*rad * hig;
+            if (volume>bigVolume){
+                bigVolume=volume;
+                bearNameBig=beerName;
+
+            }
+            beerNum--;
+        }
+        System.out.println(bearNameBig);
+
+    }
+}
