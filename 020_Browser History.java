119885378494a9ab8098c782cf4124502e1c8f77
Deyan Malinov <71469670+deyanmalinov@users.noreply.github.com>
2023-07-24 20:53:38 +0300

    Add files via upload

---
+++ 020_Browser History.java
@@ -0,0 +1,25 @@
+package DPM;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        ArrayDeque<String> web = new ArrayDeque<>();
+       String line ;
+
+        while (!"Home".equalsIgnoreCase(line = scan.nextLine())){
+            if (!"back".equalsIgnoreCase(line)){
+                web.push(line);
+                System.out.println(line);
+                }else {
+                if (web.size()>1){
+                    web.pop();
+                    System.out.println(web.peek());
+                }else {
+                    System.out.println("no previous URLs");
+                }
+            }
+
+        }
+    }
+}
