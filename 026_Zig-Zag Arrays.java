af34e65734acf06fb0674548f115b53502fb2df7
Deyan <deyanm@gmail.com>
2023-08-09 23:19:30 +0300

    empty

---
+++ 026_Zig-Zag Arrays.java
@@ -0,0 +1,35 @@
+package DPM;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lines = scan.nextInt();
+
+        int [] fArr = new int[lines];
+        int [] sArr = new int[lines];
+
+        for (int i = 0; i < lines; i++) {
+           int pChis = scan.nextInt();
+           int vtChis = scan.nextInt();
+           if(i==0) {
+               fArr[i] = pChis;
+               sArr[i] = vtChis;
+           }else if (i%2!=0) {
+               fArr[i] = vtChis;
+               sArr[i] = pChis;
+           }else {
+               fArr[i] = pChis;
+               sArr[i] = vtChis;
+           }
+        }
+        for (int ff:fArr) {
+            System.out.print(ff+" ");
+
+        }
+        System.out.println();
+        for (int ss:sArr) {
+            System.out.print(ss+" ");
+
+        }
+    }
+}
