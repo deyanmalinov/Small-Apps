05b94185e52164960e22f0223b260366edb576b4
Deyan <deyanm@gmail.com>
2023-09-14 20:18:24 +0300

    empty

---
+++ 035_Consumer Print - Functional Programming - Exercises.java
@@ -0,0 +1,14 @@
+package DPM;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split("\\s+");
+        Consumer<String > print = System.out::println;
+        Arrays.stream(line).forEach(print);
+    }
+}
