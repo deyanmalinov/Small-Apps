c46f1df6017cc6322ecdae2227ac4fecce7b8f36
Deyan <deyanm@gmail.com>
2023-09-20 00:59:13 +0300

    empty

---
+++ Min Even Number.java
@@ -0,0 +1,28 @@
+package DPM;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Optional<Double> nums = Arrays.stream(scan.nextLine().split(" "))
+                .filter(num -> !num.isEmpty())
+                .map(Double::valueOf)
+                .filter(num -> num % 2 == 0)
+                .min(Double::compare);
+----------------------------------------------------------------
+        OptionalDouble nums = Arrays.stream(scan.nextLine().split(" "))
+                .filter(num -> !num.isEmpty())
+                .mapToDouble(Double::parseDouble)
+                .filter(num -> num % 2 == 0)
+                .min();
+-----------------------------------------------------------------
+        if (nums.isPresent()){
+            System.out.printf("%.2f",nums.get()); //nums.getAsDouble();
+        }else {
+            System.out.println("No match");
+        }
+    }
+}
