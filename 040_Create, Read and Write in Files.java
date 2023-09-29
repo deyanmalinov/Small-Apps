ad26a33d40e54d8ee1d4de908307c85e0acf0825
Deyan <deyanm@gmail.com>
2023-09-30 00:26:10 +0300

    empty

---
+++ 040_Create, Read and Write in Files.java
@@ -0,0 +1,27 @@
+package DPM;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+        public static void main(String[] args) throws IOException {
+                File file = new File(FILE_PATH);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null){
+                        System.out.println(line);
+                }
+//                while (true){
+//                        String line = reader.readLine();
+//                        if (line == null){
+//                                break;
+//                        }
+//
+//                        System.out.println(line);
+//              }
+
+        }
+        private static final String FILE_PATH = "D://Coding/Java/Softuni/Java Fundamentals  2016/3. Java-Streams/users.txt";
+}
