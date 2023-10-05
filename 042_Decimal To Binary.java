9a62c196ffcde10be0423e78e385823227ed8bbb
Deyan <deyanm@gmail.com>
2023-10-05 23:03:39 +0300

    empty

---
+++ 042_Decimal To Binary.java
@@ -0,0 +1,24 @@
+package DPM;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num == 0){
+            System.out.println(0);
+            return;
+        }
+        Deque<Integer> stack = new ArrayDeque<>();
+        while (num !=0){
+            stack.push(num % 2);
+            num /= 2;
+        }
+        for (Integer integer : stack) {
+            System.out.print(stack.pop());
+
+        }
+
+        }
+}
