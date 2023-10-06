b4ac85a77f64bc122af6bf1d12273d2f643f3546
Deyan <deyanm@gmail.com>
2023-10-06 09:56:19 +0300

    empty

---
+++ 043_Define Bank Account Class.java
@@ -0,0 +1,44 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+ 		acc.setId(1);
+        acc.setBalance(15);
+        acc.withdraw(5);
+ 		System.out.printf("Account ID%d, balance %.2f"
+                ,acc.getId(),acc.getBalance());
+	}
+}
+--------------------------------------
+package com.company;
+public class BankAccount {
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public void deposit(double amaunt){
+        this.balance+=amaunt;
+    }
+    public void withdraw(double amaunt){
+        this.balance-=amaunt;
+    }
+    private double amaunt;
+    private int id;
+    private double balance;
+    @Override
+    public String toString(){
+        return "ID" + this.id;
+    }
+    public BankAccount() {
+        this.id = id;
+        this.balance = balance;
+    }
+}
