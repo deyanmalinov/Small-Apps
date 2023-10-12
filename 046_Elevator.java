446a452158b4ed8aba50ac46867982f2b03ecbf8
Deyan <deyanm@gmail.com>
2023-10-12 20:24:35 +0300

    empty

---
+++ 046_Elevator.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int numPeople = Integer.parseInt(scan.nextLine());
+            int capasiti = Integer.parseInt(scan.nextLine());
+
+            int cours = numPeople / capasiti;
+            int cous = numPeople % capasiti;
+            if (cous!=0){
+                cours++;
+            }
+            System.out.println(cours);
+    }
+
+}
