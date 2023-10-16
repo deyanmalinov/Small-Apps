15f4c22244ab0c2b6ecd0ff5cef2b2aa206aa135
Deyan <deyanm@gmail.com>
2023-10-16 23:10:26 +0300

    empty

---
+++ 049_Even and Odd Subtraction.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String line = scan.nextLine();
+            String [] arr = line.split(" ");
+            int [] newArr = new int[arr.length];
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < newArr.length; i++) {
+                newArr[i] = Integer.parseInt(arr[i]);
+                if (newArr[i] % 2 == 0){
+                    evenSum += newArr[i];
+                }else {
+                    oddSum += newArr[i];
+                }
+
+            }
+            System.out.println(evenSum-oddSum);
+            
+
+        }
+
+}
