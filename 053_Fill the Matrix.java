c79c8efeacc2bbe9217e696b55a23c297a90103e
Deyan <deyanm@gmail.com>
2023-10-26 21:40:59 +0300

    empty

--- 053_Fill the Matrix.java
+++ 053_Fill the Matrix.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(", ");
+
         int  size = Integer.parseInt(line[0]);
+
         String type = line[1];
+
         int[][] matrix = new int[size][size];
+
         int fill= 1;
+
         for (int i = 0; i < size; i++) {
             if (i%2==0){
                 for (int j = 0; j < size; j++) {
