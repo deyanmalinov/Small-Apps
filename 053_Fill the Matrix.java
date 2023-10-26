44c6b2f678d6a09fd4c64dcaff7810764f59579a
Deyan <deyanm@gmail.com>
2023-10-26 21:39:26 +0300

    empty

---
+++ 053_Fill the Matrix.java
@@ -0,0 +1,25 @@
+package DPM;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] line = scan.nextLine().split(", ");
+        int  size = Integer.parseInt(line[0]);
+        String type = line[1];
+        int[][] matrix = new int[size][size];
+        int fill= 1;
+        for (int i = 0; i < size; i++) {
+            if (i%2==0){
+                for (int j = 0; j < size; j++) {
+                    matrix[j][i] = fill++;
+                }
+            }else {
+                for (int j = matrix.length-1; j >= 0; j--) {
+                    matrix[j][i] = fill++;
+                }
+            }
+        }
+    }
+}
