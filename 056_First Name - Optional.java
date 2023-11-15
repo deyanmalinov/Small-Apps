ed198367e0220373ce81417f158a8200772b92fa
Deyan Malinov <deyanm@gmail.com>
2023-11-15 09:42:45 +0200

    empty

---
+++ 056_First Name - Optional.java
@@ -0,0 +1,26 @@
+package DPM;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<String> names = Arrays.asList(scan.nextLine().split(" "));
+        HashSet<Character> letters = new HashSet<>();
+
+        Stream.of(scan.nextLine()
+                .split(" "))
+                .map(ch -> ch.toLowerCase().charAt(0))
+                .forEach(ch -> letters.add(ch));
+        Optional<String> name = names.stream()
+                .filter(str -> letters.contains(str.toLowerCase().charAt(0)))
+                .sorted()
+                .findFirst();
+        if (name.isPresent()){
+            System.out.println(name.get());
+        }else {
+            System.out.println("No match");
+        }
+    }
+}
