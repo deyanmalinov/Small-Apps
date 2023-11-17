7ae6f723646f591df810cd31717c87ef9692ea4a
Deyan Malinov <deyanm@gmail.com>
2023-11-17 20:57:42 +0200

    empty

--- 057_Furniture - While - scan.java
+++ 057_Furniture - While - scan.java
@@ -9,14 +9,14 @@ public class Main {
             Scanner scan = new Scanner(System.in);
            String regex = "\\>\\>(?<one>[A-Za-z]+)\\<\\<(?<two>[\\d\\.]+)!(?<three>[\\d]+)";
             List<String> mebel = new ArrayList<>();
-///////////////////////////////////
+
             String string = scan.nextLine();
             double sum =0;
 
             while (!"Purchase".equals(string)){
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(string);
-//////////////////////////////////////
+
             while (matcher.find()) {
                 mebel.add(matcher.group("one"));
                 double cena = Double.parseDouble(matcher.group("two"));
