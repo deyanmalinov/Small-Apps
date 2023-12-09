bfcf66af64e05f029bb730aec54b84e32c0c7c46
deyanmalinov <deyanm@gmail.com>
2023-12-09 15:38:19 +0200

    empty

--- 059_bankAccount.java
+++ 059_bankAccount.java
@@ -1,8 +1,6 @@
 package com.DPM;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class bankAccount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
