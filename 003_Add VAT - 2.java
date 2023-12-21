47a2fa0a7b93aaef01039b7313607bcf9b0c5ea5
deyanmalinov <deyanm@gmail.com>
2023-12-21 19:27:25 +0200

    empty

--- 003_Add VAT - 2.java
+++ 003_Add VAT - 2.java
@@ -5,10 +5,15 @@ import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(", ");
+
         UnaryOperator<Double> addVat = d -> 1.2 * d;
+
         System.out.println("Prices with VAT:");
+
         Arrays.stream(line).map(Double::parseDouble)
                 .map(addVat)
                 .forEach(num -> System.out.printf("%.2f\n", num));
