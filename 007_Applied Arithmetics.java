3fa569f536bc2bb1b5b0e6cd6bb536b873057dc3
deyanmalinov <deyanm@gmail.com>
2023-12-22 18:58:24 +0200

    empty

--- 007_Applied Arithmetics.java
+++ 007_Applied Arithmetics.java
@@ -1,4 +1,5 @@
 package com.DPM;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Consumer;
@@ -6,15 +7,24 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int[] nums = Arrays.stream(scan.nextLine().split("\\s+"))
+
+        int[] nums = Arrays.stream(scan.nextLine()
+                .split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
+
         Function<int[], int[]> add = arr -> Arrays.stream(arr).map(el -> ++el).toArray();
+
         Function<int[], int[]> sub = arr -> Arrays.stream(arr).map(el -> --el).toArray();
+
         Function<int[], int[]> mult = arr -> Arrays.stream(arr).map(el -> el *= 2).toArray();
+
         Consumer<int[]> print = arr -> Arrays.stream(arr).forEach(el -> System.out.print(el + " "));
+
         String comand = scan.nextLine();
+
         while (!comand.equals("end")){
             switch (comand){
                 case "add":
