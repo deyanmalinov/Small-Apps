664ac14b542cd69a04ecbdd312aa64b51216fe38
Deyan <deyanm@gmail.com>
2024-01-05 13:31:40 +0200

    Test

--- 049_Even and Odd Subtraction.java
+++ 049_Even and Odd Subtraction.java
@@ -4,11 +4,17 @@ import java.util.*;
 
 public class Main {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String line = scan.nextLine();
+
             String [] arr = line.split(" ");
+
             int [] newArr = new int[arr.length];
+
             int oddSum = 0;
+
             int evenSum = 0;
 
             for (int i = 0; i < newArr.length; i++) {
