6da456ae8fcc8553405914819af98cf8621522ce
Deyan <deyanm@gmail.com>
2024-01-07 03:14:11 +0200

    Test

---
+++ arraysPrint.java
@@ -0,0 +1,19 @@
+public class arraysPrint {
+    public static void main(String[] args) {
+        int[] array = arrayOfMultiples(2, 3);
+        for (int i : array) {
+            System.out.print(i);
+        }
+    }
+
+    public static int[] arrayOfMultiples(int num, int length) {
+            int[] nums = new int[length];
+
+            int sum = 0;
+            for (int i=0; i <length; i++) {
+                sum = sum + num;
+                nums[i] = sum;
+            }
+            return nums;
+    }
+}
