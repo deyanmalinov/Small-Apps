46d765822e1544d112a4cfbe8015adcacc0c2893
Deyan <deyanm@gmail.com>
2024-01-08 03:46:43 +0200

    Test

---
+++ warOfNumbers.java
@@ -0,0 +1,19 @@
+public class warOfNumbers {
+    public static void main(String... args) {
+        int result = 0;
+        int sumEven = 0;
+        int sumOdd = 0;
+        int[] array = {2, 8, 6, 3, 9, 1};
+        for (int num : array) {
+            if (num % 2 == 0) {
+                sumEven += num;
+            } else {
+                sumOdd += num;
+            }
+        }
+        System.out.println(sumEven);
+        System.out.println(sumOdd);
+        result = Math.abs(sumEven - sumOdd);
+        System.out.println(result);
+    }
+}
