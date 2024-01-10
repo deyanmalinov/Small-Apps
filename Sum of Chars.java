afc8cd1f5ca7e5b7d5b77d3b3ad1eeb45b30d59e
Deyan <deyanm@gmail.com>
2024-01-10 13:54:30 +0200

    Test

--- Sum of Chars.java
+++ Sum of Chars.java
@@ -4,9 +4,13 @@ import java.util.*;
 
 public class Main {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             int lines = Integer.parseInt(scan.nextLine());
+
             int sum = 0;
+
             for (int i = 0; i < lines; i++) {
                 String chark = scan.nextLine();
                 char znak = chark.charAt(0);
