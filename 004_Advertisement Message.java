59439fe5979044dc38ae1ce0c32db96cac6feec9
deyan <deyanm@gmail.com>
2024-01-21 22:16:54 +0200

    Update

--- 004_Advertisement Message.java
+++ 004_Advertisement Message.java
@@ -8,14 +8,18 @@ public class Main {
 
             int num = Integer.parseInt(scan.nextLine());
 
-            String [] phrases = {"Excellent product.", "Such a great product.", "I always use that product.",
-                    "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+            String [] phrases = {"Excellent product.", "Such a great product.",
+                    "I always use that product.",
+                    "Best product of its category.", "Exceptional product.",
+                    "I can’t live without this product."};
 
             String [] events = {"Now I feel good.", "I have succeeded with this product.",
-                    "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome." +
+                    "Makes miracles. I am happy of the results!",
+                    "I cannot believe but now I feel awesome." +
                     "", "Try it yourself, I am very satisfied.", "I feel great!"};
 
-            String [] authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+            String [] authors = {"Diana", "Petya", "Stella",
+                    "Elena", "Katya", "Iva", "Annie", "Eva"};
 
             String [] cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
 
@@ -23,8 +27,10 @@ public class Main {
 
             for (int i = 0; i < num; i++) {
                 System.out.printf("%s %s %s - %s\n",
-                        phrases[rnd.nextInt(phrases.length)], events[rnd.nextInt(events.length)],
-                        authors[rnd.nextInt(authors.length)], cities[rnd.nextInt(cities.length)]);
+                        phrases[rnd.nextInt(phrases.length)],
+                        events[rnd.nextInt(events.length)],
+                        authors[rnd.nextInt(authors.length)],
+                        cities[rnd.nextInt(cities.length)]);
 
             }
         }
