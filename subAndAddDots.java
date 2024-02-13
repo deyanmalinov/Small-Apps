99c1f717a0966757a9c3133d0548fceea7a10d71
Deyan <deyanm@gmail.com>
2024-02-13 02:02:51 +0200

    Test

---
+++ subAndAddDots.java
@@ -0,0 +1,8 @@
+package com.DPM;
+
+public class subAndAddDots {
+    public static void main(String[] args) {
+        String word = "text";
+        System.out.println(word.substring(0,2)+ "...");
+    }
+}
