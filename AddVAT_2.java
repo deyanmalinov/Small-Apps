ac7f39c7f1876b3cf1da51645e5aa644af935208
DPM <deyanm@gmail.com>
2024-02-29 01:33:03 +0200

    Update

---
+++ AddVAT_2.java
@@ -0,0 +1,21 @@
+package com.DPM;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
+public class AddVAT_2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] line = scan.nextLine().split(", ");
+
+        UnaryOperator<Double> addVat = d -> 1.2 * d;
+
+        System.out.println("Prices with VAT:");
+
+        Arrays.stream(line).map(Double::parseDouble)
+                .map(addVat)
+                .forEach(num -> System.out.printf("%.2f\n", num));
+    }
+}
