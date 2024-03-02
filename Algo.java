6a53559c6e9b9aacd2bdbea049fd3eeef3512bbd
DPM <deyanm@gmail.com>
2024-03-02 10:14:01 +0200

    Simple Java Apps

--- Algo.java
+++ Algo.java
@@ -1,90 +1,56 @@
-package com.DPM;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Algo {
-    public static void main(String[] args) {
-        private final int v;
+public class AllPathsFromSourceToTarget {
+    private final int vertices;
+    private List<List<Integer>> nm = new ArrayList<>();
+    private List<Integer>[] adjList;
 
-        // To store the paths from source to destination
-        static List<List<Integer>> nm = new ArrayList<>();
-        // adjacency list
-        private ArrayList<Integer>[] adjList;
-
-        // Constructor
     public AllPathsFromSourceToTarget(int vertices) {
+        this.vertices = vertices;
+        initAdjList();
+    }
 
-            // initialise vertex count
-            this.v = vertices;
-
-            // initialise adjacency list
-            initAdjList();
+    private void initAdjList() {
+        adjList = new ArrayList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjList[i] = new ArrayList<>();
         }
+    }
 
-        // utility method to initialise adjacency list
-        private void initAdjList() {
-            adjList = new ArrayList[v];
+    public void addEdge(int u, int v) {
+        adjList[u].add(v);
+    }
 
-            for (int i = 0; i < v; i++) {
-                adjList[i] = new ArrayList<>();
+    public List<List<Integer>> storeAllPaths(int source, int destination) {
+        boolean[] isVisited = new boolean[vertices];
+        ArrayList<Integer> pathList = new ArrayList<>();
+        pathList.add(source);
+        storeAllPathsUtil(source, destination, isVisited, pathList);
+        return nm;
+    }
+
+    private void storeAllPathsUtil(int u, int d, boolean[] isVisited, List<Integer> localPathList) {
+        if (u == d) {
+            nm.add(new ArrayList<>(localPathList));
+            return;
+        }
+        isVisited[u] = true;
+        for (Integer i : adjList[u]) {
+            if (!isVisited[i]) {
+                localPathList.add(i);
+                storeAllPathsUtil(i, d, isVisited, localPathList);
+                localPathList.remove(localPathList.size() - 1);
             }
         }
+        isVisited[u] = false;
+    }
 
-        // add edge from u to v
-        public void addEdge(int u, int v) {
-            // Add v to u's list.
-            adjList[u].add(v);
+    public static List<List<Integer>> allPathsFromSourceToTarget(int vertices, int[][] edges, int source, int destination) {
+        AllPathsFromSourceToTarget graph = new AllPathsFromSourceToTarget(vertices);
+        for (int[] edge : edges) {
+            graph.addEdge(edge[0], edge[1]);
         }
-
-        public void storeAllPaths(int s, int d) {
-            boolean[] isVisited = new boolean[v];
-            ArrayList<Integer> pathList = new ArrayList<>();
-
-            // add source to path[]
-            pathList.add(s);
-            // Call recursive utility
-            storeAllPathsUtil(s, d, isVisited, pathList);
-        }
-
-        // A recursive function to print all paths from 'u' to 'd'.
-        // isVisited[] keeps track of vertices in current path.
-        // localPathList<> stores actual vertices in the current path
-        private void storeAllPathsUtil(Integer u, Integer d, boolean[] isVisited, List<Integer> localPathList) {
-
-            if (u.equals(d)) {
-                nm.add(new ArrayList<>(localPathList));
-                return;
-            }
-
-            // Mark the current node
-            isVisited[u] = true;
-
-            // Recursion for all the vertices adjacent to current vertex
-
-            for (Integer i : adjList[u]) {
-                if (!isVisited[i]) {
-                    // store current node in path[]
-                    localPathList.add(i);
-                    storeAllPathsUtil(i, d, isVisited, localPathList);
-
-                    // remove current node in path[]
-                    localPathList.remove(i);
-                }
-            }
-
-            // Mark the current node
-            isVisited[u] = false;
-        }
-
-        // Driver program
-        public static List<List<Integer>> allPathsFromSourceToTarget(int vertices, int[][] a, int source, int destination) {
-            // Create a sample graph
-            AllPathsFromSourceToTarget g = new AllPathsFromSourceToTarget(vertices);
-            for (int[] i : a) {
-                g.addEdge(i[0], i[1]);
-                // edges are added
-            }
-            g.storeAllPaths(source, destination);
-            // method call to store all possible paths
-            return nm;
-            // returns all possible paths from source to destination
+        return graph.storeAllPaths(source, destination);
     }
 }
