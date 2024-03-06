445501b13310881efdaa628726ccf6e8f3f11740
DPM <deyanm@gmail.com>
2024-03-06 05:32:27 +0200

    Update

--- AMinerTask.java
+++ AMinerTask.java
@@ -1,14 +1,14 @@
 package com.DPM;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
-
 public class AMinerTask {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         Map<String, Integer> goods = new LinkedHashMap<>();
+
         String line = "";
 
         while (!"stop".equals(line = scan.nextLine())){
