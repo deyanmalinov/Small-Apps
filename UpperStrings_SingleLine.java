1f139b5d83292b156d607456693e45903f055a24
DPM <deyanm@gmail.com>
2024-03-08 02:55:12 +0200

    Update

--- UpperStrings_SingleLine.java
+++ UpperStrings_SingleLine.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Main {
+public class UpperStrings_SingleLine {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
