738b2b4550b1c1c87f97734c6a7b182975d6b97a
DPM <deyanm@gmail.com>
2024-03-13 06:23:25 +0200

    Update

--- SimpleCalculator2.java
+++ SimpleCalculator2.java
@@ -1,18 +1,23 @@
 package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
-
 public class SimpleCalculator2 {
     public static void main(String[] args) {
+
         //2 + 5 + 10 - 2 - 1
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(" ");
+
         ArrayDeque<String> stack = new ArrayDeque<>();
+
         //Collections.addAll(stack, line);
+
         for (int i = line.length-1; i > -1; i--) {
             stack.push(line[i]);
         }
-       while (stack.size()>1){
+        while (stack.size()>1){
            int a = Integer.parseInt(stack.pop());
            String znak = stack.pop();
            int b = Integer.parseInt(stack.pop());
