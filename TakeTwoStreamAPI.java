4ed45cae9145fa2acf4c8b18cc8e80412576d05e
DPM <deyanm@gmail.com>
2024-03-14 02:51:06 +0200

    Update

--- TakeTwoStreamAPI.java
+++ TakeTwoStreamAPI.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Main {
+public class TakeTwoStreamAPI {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         List<Integer> nums = Arrays.stream(scan.nextLine()
