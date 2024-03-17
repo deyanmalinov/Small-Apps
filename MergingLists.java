e51c1d6ab950a3c90b11cc08eee6367ea28891f2
DPM <deyanm@gmail.com>
2024-03-17 06:11:41 +0200

    Update

--- MergingLists.java
+++ MergingLists.java
@@ -3,26 +3,26 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class MergingLists {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
 
             List<Integer> listArr1 = Arrays.stream(scan.nextLine().split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
+
             List<Integer> listArr2 = Arrays.stream(scan.nextLine().split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
+
             int size = Math.min(listArr1.size(), listArr2.size());
 
             List <Integer> finalList = new ArrayList<>();
-
 
             for (int i = 0; i < size; i++) {
 
                 finalList.add(listArr1.get(i));
 
                 finalList.add(listArr2.get(i));
-
-
             }
             if (listArr1.size()>listArr2.size()){
                 finalList.addAll(listArr1.subList(size, listArr1.size()));
