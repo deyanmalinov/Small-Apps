cf0b3aa2c71302b7f055d3a7cca1b524c752d927
DPM <deyanm@gmail.com>
2024-03-26 06:29:51 +0200

    Update

--- SumAdjacentEqualNumbers.java
+++ SumAdjacentEqualNumbers.java
@@ -2,17 +2,20 @@ package com.DPM;
 import java.text.DecimalFormat;
 import java.util.*;
 public class SumAdjacentEqualNumbers {
+
     private int nums;
 
     public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String [] strArr = scan.nextLine().split(" ");
+
             ArrayList<Double> intList = new ArrayList<>();
 
             for (String num : strArr) {
-            intList.add(Double.parseDouble(num));
-
-        }
+               intList.add(Double.parseDouble(num));
+            }
 //			List<Double> intList = Arrays.stream(scan.nextLine().split(" "))
 //			.map(Double::parseDouble).collect(Collectors.toList());
 
@@ -20,24 +23,16 @@ public class SumAdjacentEqualNumbers {
 //                double num = Double.parseDouble(strArr[i]);
 //                intList.add(num);
 //            }
-
-            for (int i = 0; i < intList.size()-1; i++) {
-
-
+        for (int i = 0; i < intList.size()-1; i++) {
             if (intList.get(i).equals(intList.get(i+1))){
                 intList.set(i, intList.get(i)+(intList.get(i+1)));
                 intList.remove(i+1);
                 i=-1;
             }
-
         }
         DecimalFormat form = new DecimalFormat("#.###");
         for (Double num : intList) {
-            
             System.out.print(form.format(num)+" ");
-
         }
-
-
     }
 }
