642eb3adb7c5442b2a4f74c5fd7d32f71fcede0c
DPM <deyanm@gmail.com>
2024-03-29 06:30:20 +0200

    Update

--- SumArrays.java
+++ SumArrays.java
@@ -1,9 +1,11 @@
 package com.DPM;
-
 public class SumArrays {
     public static void main(String[] args) {
+
         int sum = 0;
+
         int[] numbers = {1, 2, 3, 4, 5};
+
         if(numbers.length>0){
             for (int i = 0; i < numbers.length; i++) {
                 sum = sum + numbers[i];
@@ -12,7 +14,9 @@ public class SumArrays {
             sum = numbers[0];
         }
 //        System.out.println(sum);
+
         sum=recurs(numbers, numbers.length);
+
         System.out.println(sum);
     }
     public static int recurs(int[] array, int n) {
