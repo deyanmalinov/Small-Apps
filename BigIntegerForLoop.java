c36ec678f4e1917b3986e88c06b4d7ef2aa5414f
DPM <deyanm@gmail.com>
2024-03-31 01:41:17 +0200

    Update

--- BigIntegerForLoop.java
+++ BigIntegerForLoop.java
@@ -4,8 +4,11 @@ import java.math.BigInteger;
 
 public class BigIntegerForLoop {
     public static void main(String[] args) {
+
         BigInteger first = new BigInteger("1");
+
         BigInteger second = new BigInteger("4");
+
         for (BigInteger i = first;
              i.compareTo(second) <= 0;
              i = i.add(BigInteger.valueOf(1))) {
