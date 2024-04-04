d35b15fbb4d643bba910611d86063d0fa62d8ab5
DPM <deyanm@gmail.com>
2024-04-04 03:00:20 +0300

    Update

--- warOfNumbers.java
+++ warOfNumbers.java
@@ -1,11 +1,12 @@
 package com.DPM;
 public class warOfNumbers {
     public static void main(String... args) {
-
         int result = 0;
         int sumEven = 0;
         int sumOdd = 0;
+
         int[] array = {2, 8, 6, 3, 9, 1};
+
         for (int num : array) {
             if (num % 2 == 0) {
                 sumEven += num;
