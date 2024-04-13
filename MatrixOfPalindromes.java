eee2ac7625c7558ffafdfedb076acd9d4f36d9da
DPM <deyanm@gmail.com>
2024-04-13 06:08:31 +0300

    Update

--- MatrixOfPalindromes.java
+++ MatrixOfPalindromes.java
@@ -2,9 +2,13 @@ package com.DPM;
 import java.util.Scanner;
 public class MatrixOfPalindromes {
     public static void main(String[] args){
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(" ");
+
         int rows = Integer.parseInt(line[0]);
+
         int cols = Integer.parseInt(line[1]);
 
         String[][] matrix = new String[rows][cols];
@@ -18,7 +22,6 @@ public class MatrixOfPalindromes {
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 System.out.print(matrix[row][col]+ " ");
-
             }
             System.out.println();
         }
