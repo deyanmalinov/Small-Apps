6555cc837bb7590aab6c4a0c03f9060b283ab359
DPM <deyanm@gmail.com>
2024-04-14 01:43:35 +0300

    Update

---
+++ CarInfo2.java
@@ -0,0 +1,49 @@
+package com.DPM;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+public class CarInfo2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        IntStream
+                .rangeClosed(1, num)
+                .boxed()
+                .map(n -> scan.nextLine().split(" "))
+                .map(data ->{
+            Cars123 newCar = new Cars123();
+            newCar.setMake(data[0]);
+            newCar.setModel(data[1]);
+            newCar.setHorsePower(Integer.parseInt(data[2]));
+            return newCar;
+        })
+                .forEach(car -> System.out.println(car.getCarInfo()));
+    }
+}
+
+class Cars123 {
+        private String make;
+        public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getHorsePower() {
+        return horsePower;
+    }
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    private String model;
+        private int horsePower;
+        public String getCarInfo(){
+            return String.format("The car is: %s %s - %d HP.",
+                    this.getMake(), this.getModel(), this.getHorsePower());
+        }
+}
---
+++ CarInfoDefiningClassesLab.java
@@ -0,0 +1,45 @@
+package com.DPM;
+import java.util.Scanner;
+public class CarInfoDefiningClassesLab {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Kolite newCar = new Kolite();
+        int num = Integer.parseInt(scan.nextLine());
+        String[] line = scan.nextLine().split("\\s+");
+
+        for (int i = 0; i < num; i++) {
+            newCar.setMake(line[0]);
+            newCar.setModel(line[1]);
+            newCar.setHorsePower(Integer.parseInt(line[2]));
+
+            System.out.printf("The car is: %s %s - %d HP.%n",
+                    newCar.getMake(), newCar.getModel(), newCar.getHorsePower());
+            line = scan.nextLine().split("\\s+");
+        }
+    }
+}
+
+class Kolite {
+        private String make;
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    private String model;
+        private int horsePower;
+}
