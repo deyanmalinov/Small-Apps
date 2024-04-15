bdaa2e01550e62fc790527b4fff39c5f0a6581c0
DPM <deyanm@gmail.com>
2024-04-15 06:26:37 +0300

    Update

--- SplitBySpace2.java
+++ SplitBySpace2.java
@@ -1,13 +1,16 @@
 package com.DPM;
-
 import java.util.*;
-
 public class SplitBySpace2 {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String nums = scan.nextLine();
+
             String [] mas = nums.split(" ");
+
             int [] chisla = new int [mas.length];
+
             int sum =0;
 
             for (int i = 0; i < chisla.length; i++) {
@@ -15,9 +18,7 @@ public class SplitBySpace2 {
                 if (chisla[i] %2 == 0){
                     sum += chisla[i];
                 }
-
             }
             System.out.println(sum);
         }
-
 }
