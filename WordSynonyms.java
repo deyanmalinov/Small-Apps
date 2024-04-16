274f8784a09ec1453f079ef563a24bcd2d395599
DPM <deyanm@gmail.com>
2024-04-16 03:01:16 +0300

    Update

--- WordSynonyms.java
+++ WordSynonyms.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class WordSynonyms {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         int num = Integer.parseInt(scan.nextLine());
+
         Map <String, List<String>> newMap = new LinkedHashMap<>();
+
         for (int i = 0; i < num; i++) {
             String str1 = scan.nextLine();
             String str2 = scan.nextLine();
             newMap.putIfAbsent(str1, new ArrayList<>());
             newMap.get(str1).add(str2);
         }
+
         for (Map.Entry<String, List<String>> entry : newMap.entrySet()) {
             String value1 = String.join(", ", entry.getValue());
             System.out.printf("%s - %s\n", entry.getKey(), value1);
