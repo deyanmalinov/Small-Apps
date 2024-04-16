c5105d7a92c9ce12a8580db11d21fcc368b112c0
DPM <deyanm@gmail.com>
2024-04-16 06:14:42 +0300

    Update

--- PalindromeIntegers.java
+++ PalindromeIntegers.java
@@ -2,17 +2,18 @@ package com.DPM;
 import java.util.*;
 public class PalindromeIntegers {
         public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            String line = "";
-            while (true){
 
+            Scanner scan = new Scanner(System.in);
+
+            String line = "";
+
+            while (true){
                 line = scan.nextLine();
                 if (line.equals("END")){
                     break;
                 }
                 for (int i = 0; i <= line.length()/2; i++) {
                     int lenth = line.length();
-
                     if (line.charAt(i) == line.charAt(lenth-1)){
                         lenth--;
                         System.out.println(true);
@@ -21,8 +22,7 @@ public class PalindromeIntegers {
                         System.out.println(false);
                         break;
                     }
-                }
-
             }
+        }
     }
 }
