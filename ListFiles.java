5baa41b4e0acc5e901dee33f93d25f186427d9cb
DPM <deyanm@gmail.com>
2024-04-21 06:04:54 +0300

    Update

--- ListFiles.java
+++ ListFiles.java
@@ -1,14 +1,15 @@
 package com.DPM;
-
 import java.io.File;
 import java.io.IOException;
-
 public class ListFiles {
     public static void main(String[] args) throws IOException {
+
         String path = "D:\\Coding\\Java\\Softuni\\Java Advanced - January 2019\\" +
                 "04. Java-Advanced-Fiels-and-Directories-Lab\\04. " +
                 "Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams";
+
         File file = new File(path);
+
         if (file.exists()) {
             if (file.isDirectory()) {
                 File[] files = file.listFiles();
