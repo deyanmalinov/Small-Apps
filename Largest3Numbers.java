2087416b5ac07ceb99270f9fb7c46604639bfc9a
DPM <deyanm@gmail.com>
2024-04-21 06:03:30 +0300

    Update

--- Largest3Numbers.java
+++ Largest3Numbers.java
@@ -1,12 +1,13 @@
 package com.DPM;
-
 import java.util.*;
 import java.util.stream.Collectors;
-
 public class Largest3Numbers {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
-            System.out.println(Arrays.stream(scan.nextLine().split(" "))
+
+            System.out.println(Arrays.stream(scan.nextLine()
+                    .split(" "))
                     .map(n -> Integer.parseInt(n))
                     .sorted(Collections.reverseOrder())
                     .limit(3)
