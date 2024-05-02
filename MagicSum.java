410cbb24f81ad9ee2d71dc35bce9e51a05d820c3
DPM <deyanm@gmail.com>
2024-05-02 06:05:37 +0300

    Update

--- MagicSum.java
+++ MagicSum.java
@@ -2,13 +2,19 @@ package com.DPM;
 import java.util.*;
 public class MagicSum {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String [] line = scan.nextLine().split(" ");
+
         int [] nums = new int[line.length];
+
         int sum = Integer.parseInt(scan.nextLine());
+
         for (int i = 0; i < nums.length; i++) {
             nums[i] = Integer.parseInt(line[i]);
         }
+
         for (int i = 0; i < nums.length-1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 int num = nums[i] + nums[j];
@@ -17,6 +23,5 @@ public class MagicSum {
                 }
             }
         }
-
     }
 }
