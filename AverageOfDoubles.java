c762045abecd906a3b5e0a45ff11317d1f664f9c
DPM <deyanm@gmail.com>
2024-05-13 05:36:35 +0300

    Update

--- AverageOfDoubles.java
+++ AverageOfDoubles.java
@@ -1,8 +1,6 @@
 package com.DPM;
-
 import java.util.*;
 import java.util.stream.Collectors;
-
 public class AverageOfDoubles {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
