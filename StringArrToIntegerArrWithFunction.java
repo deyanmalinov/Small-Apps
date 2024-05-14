068911931fe87b8e326b473d5dcdb4aee0abe55b
DPM <deyanm@gmail.com>
2024-05-14 06:27:06 +0300

    Update

--- StringArrToIntegerArrWithFunction.java
+++ StringArrToIntegerArrWithFunction.java
@@ -1,13 +1,17 @@
 package com.DPM;
 import java.util.Scanner;
 import java.util.function.Function;
-
 public class StringArrToIntegerArrWithFunction {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(" ");
+
         Function<String, Integer> parse = str -> Integer.parseInt(str);
+
         Integer[] newArr = new Integer[line.length];
+
         for (int i = 0; i < line.length; i++) {
             newArr[i] = parse.apply(line[i]);
         }
