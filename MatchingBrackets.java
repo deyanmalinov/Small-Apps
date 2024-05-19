f23f24ffbe41a771f6fa1feabd58eb4c64670814
DPM <deyanm@gmail.com>
2024-05-19 06:07:08 +0300

    Update

--- MatchingBrackets.java
+++ MatchingBrackets.java
@@ -1,25 +1,24 @@
 package com.DPM;
-
 import java.util.*;
-
 public class MatchingBrackets {
     public static void main(String[] args){
+
         Scanner scan = new Scanner(System.in);
+
         String line = scan.nextLine();
+
         Deque<Integer> stack = new ArrayDeque<>();
+
         for (int i = 0; i < line.length(); i++) {
             char ch = line.charAt(i);
-
             if (ch == '(') {
                 stack.push(i);
             }
             else if (ch == ')') {
                 int startIndex = stack.pop();
-                String contents =
-                        line.substring(startIndex, i + 1);
+                String contents = line.substring(startIndex, i + 1);
                 System.out.println(contents);
             }
-
         }
-        }
+     }
 }
