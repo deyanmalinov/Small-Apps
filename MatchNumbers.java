56ceed1617350121b13b3c3f8563e446a5c772ac
DPM <deyanm@gmail.com>
2024-05-19 06:07:19 +0300

    Update

--- MatchNumbers.java
+++ MatchNumbers.java
@@ -1,18 +1,22 @@
 package com.DPM;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class MatchNumbers {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String string = scan.nextLine();
+
             String regex = "(^|(?<=\\s))-?\\d+(\\.\\d+)?($|(?=\\s))";
+
             Pattern pattern = Pattern.compile(regex);
+
             Matcher line = pattern.matcher(string);
+
             List <String> nums = new LinkedList<>();
 
             while (line.find()) {
