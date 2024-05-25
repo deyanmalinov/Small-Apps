1998d1a2c3014e69fdfee7b87750f5283806d342
DPM <deyanm@gmail.com>
2024-05-25 06:06:41 +0300

    Update

--- MatchFullName.java
+++ MatchFullName.java
@@ -1,22 +1,22 @@
 package com.DPM;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class MatchFullName {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+
             String string = scan.nextLine();
 
             Pattern pattern = Pattern.compile(regex);
+
             Matcher matcher = pattern.matcher(string);
 
             while (matcher.find()) {
                 System.out.print(matcher.group(0)+" ");
-
             }
-
         }
 }
