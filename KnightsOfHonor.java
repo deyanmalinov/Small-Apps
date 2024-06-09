52a9ec09c9574dc6ef78638872d6708590dfd0b3
DPM <deyanm@gmail.com>
2024-06-09 06:02:53 +0300

    Update

--- KnightsOfHonor.java
+++ KnightsOfHonor.java
@@ -1,14 +1,16 @@
 package com.DPM;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Consumer;
-
 public class KnightsOfHonor {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split("\\s+");
+
         Consumer<String > print = word -> System.out.println("Sir " +word);
+
         Arrays.stream(line).forEach(print);
     }
 }
