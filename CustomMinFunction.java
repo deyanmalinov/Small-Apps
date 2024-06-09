cdb4ae9152201a4a8272d988f917a7e2ed146f23
DPM <deyanm@gmail.com>
2024-06-10 01:51:19 +0300

    Update

--- CustomMinFunction.java
+++ CustomMinFunction.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Function;
 
-public class Main {
+public class CustomMinFunction {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] nums = Arrays.stream(scan.nextLine().split("\\s+"))
