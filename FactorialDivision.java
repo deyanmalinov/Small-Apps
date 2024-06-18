49de91731103375a67afcacd627f511a866c2532
DPM <deyanm@gmail.com>
2024-06-19 01:55:22 +0300

    Update

--- FactorialDivision.java
+++ FactorialDivision.java
@@ -2,7 +2,7 @@ package com.DPM;
 
 import java.util.*;
 
-public class Main {
+public class FactorialDivision {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             double numOne = Double.parseDouble(scan.nextLine());
--- FillTheMatrix.java
+++ FillTheMatrix.java
@@ -2,7 +2,7 @@ package com.DPM;
 
 import java.util.Scanner;
 
-public class Main {
+public class FillTheMatrix {
 
     public static void main(String[] args) {
 
