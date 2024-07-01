f61fdaf1f086e8e2fce1ce4e60b57acdb0e9e860
DPM <deyanm@gmail.com>
2024-07-01 06:15:35 +0300

    Update

--- PartyProfit.java
+++ PartyProfit.java
@@ -1,11 +1,12 @@
 package com.DPM;
-
 import java.util.*;
-
 public class PartyProfit {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             int party = Integer.parseInt(scan.nextLine());
+
             int days = Integer.parseInt(scan.nextLine());
 
             int coin = days*50;
@@ -27,15 +28,9 @@ public class PartyProfit {
                     if (i % 3 == 0){
                         coin -= party*2;
                     }
-
                 }
-
             }
             double fin = Math.floor(coin/party);
-
             System.out.printf("%d companions received %.0f coins each.", party, fin);
-
-
         }
-
 }
