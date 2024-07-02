b3eb81d855668aa4f2d787c385683a4b8c769223
DPM <deyanm@gmail.com>
2024-07-03 02:04:25 +0300

    Update

--- FuzzBuzz.java
+++ FuzzBuzz.java
@@ -2,6 +2,7 @@ package com.DPM;
 
 public class FuzzBuzz {
     public static void main(String[] args) {
+
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
