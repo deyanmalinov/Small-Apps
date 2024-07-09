4055ef5b423a0d2c7c0bfdd1f387d153c4db7dd2
DPM <deyanm@gmail.com>
2024-07-09 06:16:09 +0300

    Update

--- PasswordValidator.java
+++ PasswordValidator.java
@@ -1,11 +1,12 @@
 package com.DPM;
-
 import java.util.*;
-
 public class PasswordValidator {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             String pass = scan.nextLine();
+
             if (validPass(pass)){
                 System.out.println("Password is valid");
             }
@@ -15,10 +16,8 @@ public class PasswordValidator {
             boolean onlyLandD = coutValid(s);
             boolean haveThoDig = twoDigVal(s);
             return correctCharCount && onlyLandD && haveThoDig;
-
         }
-
-    private static boolean twoDigVal(String s) {
+        private static boolean twoDigVal(String s) {
             int digCout = 0;
         for (int i = 0; i < s.length(); i++) {
             char sim = s.charAt(i);
@@ -33,7 +32,6 @@ public class PasswordValidator {
         System.out.println("Password must have at least 2 digits");
         return false;
     }
-
     private static boolean coutValid(String s) {
         for (int i = 0; i < s.length(); i++) {
             char simb = s.charAt(i);
@@ -41,11 +39,9 @@ public class PasswordValidator {
                 System.out.println("Password must consist only of letters and digits");
                 return false;
             }
-
         }
         return true;
     }
-
     private static boolean validCount(String s) {
             boolean isValid = s.length() > 5 && s.length() < 11;
             if (!isValid){
