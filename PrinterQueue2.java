91b2238f5cca60c24130c12de3a8f3a633f08667
DPM <deyanm@gmail.com>
2024-07-11 06:17:20 +0300

    Update

--- PrinterQueue2.java
+++ PrinterQueue2.java
@@ -1,17 +1,19 @@
 package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
-
 public class PrinterQueue2 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         ArrayDeque<String> queue = new ArrayDeque<>();
+
         String comand = scan.nextLine();
 
-        while (!"print".equals(comand)){
-            if ("cancel".equals(comand)){
+        while (!"Print".equals(comand)){
+            if ("Cancel".equals(comand)){
                 if (queue.isEmpty()){
-                    System.out.println("Printer is on standby");
+                    System.out.println("Printer is on Standby");
                 }else {
                     System.out.println("Canceled "+ queue.poll());
                 }
