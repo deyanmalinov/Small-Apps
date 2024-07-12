8b4a384ea47bf991524093a030125e90d531f6b1
DPM <deyanm@gmail.com>
2024-07-12 06:18:54 +0300

    Update

--- RandomizeWords.java
+++ RandomizeWords.java
@@ -1,18 +1,21 @@
 package com.DPM;
 import java.util.*;
-
 public class RandomizeWords {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String [] line = scan.nextLine().split(" ");
+
         List<String> fpD = new ArrayList<>();
+
         for (int i = 0; i < line.length; i++) {
             fpD.add(i, line[i]);
         }
 //        Collections.shuffle(fpD);
 //        System.out.println(String.join(System.lineSeparator(), fpD));
-
         Random rnd = new Random();
+
         while (!fpD.isEmpty()){
             int num = rnd.nextInt(fpD.size());
             System.out.println(fpD.get(num));
