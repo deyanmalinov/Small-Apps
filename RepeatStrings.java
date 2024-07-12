5fb88cfd004e16620aba35886fd205ec0afe6a26
DPM <deyanm@gmail.com>
2024-07-12 06:19:27 +0300

    Update

--- RepeatStrings.java
+++ RepeatStrings.java
@@ -1,11 +1,12 @@
 package com.DPM;
-
 import java.util.*;
-
 public class RepeatStrings {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String [] line = scan.nextLine().split(" ");
+
         for (int i = 0; i < line.length; i++) {
             String ele = line[i];
             int lenght = ele.length();
