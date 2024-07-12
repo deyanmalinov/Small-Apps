28b52aa878cbcd89352237e8d056d7062a841cba
DPM <deyanm@gmail.com>
2024-07-12 06:19:13 +0300

    Update

--- RecursiveFibonacci.java
+++ RecursiveFibonacci.java
@@ -1,16 +1,19 @@
 package com.DPM;
 import java.util.Scanner;
-
 public class RecursiveFibonacci {
     private static long[] memory;
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         int number = Integer.parseInt(scan.nextLine());
+
         memory = new long[number+1];
+
         long res = getFib(number);
+
         System.out.println(res);
     }
-
     private static long getFib(int number) {
         if (number < 2){
             return 1;
