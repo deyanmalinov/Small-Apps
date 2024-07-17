25e9be5035961d2e66267d20b2a6595bf041ad05
DPM <deyanm@gmail.com>
2024-07-17 06:16:50 +0300

    Update

--- PrintDiagonalsOfSquareMatrix.java
+++ PrintDiagonalsOfSquareMatrix.java
@@ -3,9 +3,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class PrintDiagonalsOfSquareMatrix {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         int size = Integer.parseInt(scan.nextLine());
+
         int[][] matrix = new int[size][size];
+
         for (int i = 0; i < size; i++) {
             int[] line = Arrays.stream(scan.nextLine().split(" ")).
                     mapToInt(Integer::parseInt).toArray();
