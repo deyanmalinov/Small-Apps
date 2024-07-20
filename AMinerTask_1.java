0e9b197fb474e7441ea56cc58399c22d41552683
DPM <deyanm@gmail.com>
2024-07-20 03:31:20 +0300

    Update

--- AMinerTask_1.java
+++ AMinerTask_1.java
@@ -2,7 +2,7 @@ package com.DPM;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
-public class AMinerTask {
+public class AMinerTask_1 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
