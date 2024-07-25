f5444cf9b5b1cf6268e68fa99a934e863818abd6
DPM <deyanm@gmail.com>
2024-07-25 05:45:35 +0300

    Update

--- ClassBox_DataValidation.java
+++ ClassBox_DataValidation.java
@@ -4,9 +4,13 @@ public class ClassBox_DataValidation {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         double length = Double.parseDouble(scan.nextLine());
+
         double width = Double.parseDouble(scan.nextLine());
+
         double height = Double.parseDouble(scan.nextLine());
+
         try {
             Box box = new Box(length, width, height);
             System.out.println(String.format("Surface Area - %.2f", box.surfaceArea()));
