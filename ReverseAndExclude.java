79cecf696cedada0093dc723b767f916adf1e6ec
DPM <deyanm@gmail.com>
2024-07-28 06:20:08 +0300

    Update

--- ReverseAndExclude.java
+++ ReverseAndExclude.java
@@ -3,18 +3,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
 public class ReverseAndExclude {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        List<Integer> nums = Arrays.stream(scan.nextLine().split("\\s+"))
+
+        List<Integer> nums = Arrays.stream(scan.nextLine()
+                .split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+
         int dev = Integer.parseInt(scan.nextLine());
+
         nums.stream()
                 .filter(num -> num % dev != 0)
                 .sorted((x,y)-> -1)
                 .forEach(num -> System.out.print(num + " "));
-
     }
 }
