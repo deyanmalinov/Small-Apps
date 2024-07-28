4f158a914a8aa5eecde3798b69b20037ac9976e2
DPM <deyanm@gmail.com>
2024-07-28 06:19:58 +0300

    Update

--- ReplaceRepeatingChars.java
+++ ReplaceRepeatingChars.java
@@ -1,14 +1,18 @@
 package com.DPM;
-
 import java.util.*;
-
 public class ReplaceRepeatingChars {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String text = scan.nextLine();
+
         String res ="";
+
         char ch = text.charAt(0);
+
         res += ch;
+
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.charAt(i);
             if (ch != symbol){
