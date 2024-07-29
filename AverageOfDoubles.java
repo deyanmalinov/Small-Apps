d021e39cf3f44197354ddf3d3677dd0e606a3644
DPM <deyanm@gmail.com>
2024-07-29 03:33:39 +0300

    Update

--- AverageOfDoubles.java
+++ AverageOfDoubles.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class AverageOfDoubles {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         
         List<Double> nums = Arrays.stream(scan.nextLine().split(" "))
