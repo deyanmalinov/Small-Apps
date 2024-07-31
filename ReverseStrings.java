1ede4b7758dc6493413009977cf004b14573bea7
DPM <deyanm@gmail.com>
2024-07-31 06:20:47 +0300

    Update

--- ReverseStrings.java
+++ ReverseStrings.java
@@ -1,23 +1,18 @@
 package com.DPM;
-
 import java.util.Scanner;
-
 public class ReverseStrings {
         public static void main(String[] args) {
 
             Scanner scan = new Scanner(System.in);
 
             String line = "";
+
             while (!"end".equals(line=scan.nextLine())){
                 String newLine = "";
-
                 for (int i = line.length()-1; i >=0 ; i--) {
                     newLine += line.charAt(i);
                 }
-
                 System.out.printf("%s = %s\n",line, newLine);
-
             }
-
         }
 }
