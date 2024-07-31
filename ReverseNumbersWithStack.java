d2bc51372fd6e1d3edfe6dbe5e93703e9d1fbf85
DPM <deyanm@gmail.com>
2024-07-31 06:20:37 +0300

    Update

--- ReverseNumbersWithStack.java
+++ ReverseNumbersWithStack.java
@@ -1,12 +1,15 @@
 package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
-
 public class ReverseNumbersWithStack {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String [] line = scan.nextLine().split(" ");
+
         ArrayDeque<Integer> nums = new ArrayDeque<>();
+
         for (int i = 0; i < line.length; i++) {
             nums.push(Integer.parseInt(line[i]));
         }
