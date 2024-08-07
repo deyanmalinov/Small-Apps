8f03ca72486ea5341d5d9927caaeae91d8c68bb5
DPM <deyanm@gmail.com>
2024-08-07 05:49:56 +0300

    Update

--- CountUppercaseWords2.java
+++ CountUppercaseWords2.java
@@ -1,17 +1,19 @@
 package com.DPM;
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.Predicate;
-
 public class CountUppercaseWords2 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] textAsList = scan.nextLine().split("\\s+");
+
         Predicate<String> checkerUpperCase =
                 word -> Character.isUpperCase(word.charAt(0));
+
         ArrayList<String> result = new ArrayList<>();
+
         for (int i = 0; i < textAsList.length; i++) {
             if (checkerUpperCase.test(textAsList[i]))
                 result.add(textAsList[i]);
