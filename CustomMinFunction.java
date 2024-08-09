6147adcb661c4d2e468374601bb47cd4f4f0439e
DPM <deyanm@gmail.com>
2024-08-09 05:50:44 +0300

    Update

--- CustomMinFunction.java
+++ CustomMinFunction.java
@@ -1,13 +1,14 @@
 package com.DPM;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Function;
-
 public class CustomMinFunction {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int[] nums = Arrays.stream(scan.nextLine().split("\\s+"))
+
+        int[] nums = Arrays.stream(scan.nextLine()
+                .split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
