1a3c8296a11e87f24f694599fb6cfdf3b6fff01c
DPM <deyanm@gmail.com>
2024-08-13 03:51:11 +0300

    Update

--- ListOperations.java
+++ ListOperations.java
@@ -46,7 +46,6 @@ public class ListOperations {
                             }
                         }
                         break;
-
                 }
                 line = scan.nextLine();
             }
