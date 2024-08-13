910d4fa146cc3c58041fb8cb04f50b017fa8a28a
DPM <deyanm@gmail.com>
2024-08-13 05:47:58 +0300

    Update

--- ConsumerPrintFunctionalProgramming_Exercises.java
+++ ConsumerPrintFunctionalProgramming_Exercises.java
@@ -1,14 +1,16 @@
 package com.DPM;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.function.Consumer;
-
 public class ConsumerPrintFunctionalProgramming_Exercises {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split("\\s+");
+
         Consumer<String > print = System.out::println;
+
         Arrays.stream(line).forEach(print);
     }
 }
