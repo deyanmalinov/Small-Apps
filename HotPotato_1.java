ba2f9e5eaabf6a7e273a11800e96a51ce5059a7a
DPM <deyanm@gmail.com>
2024-08-13 03:49:45 +0300

    Update

--- HotPotato_1.java
+++ HotPotato_1.java
@@ -1,7 +1,7 @@
 package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
-public class HotPotato {
+public class HotPotato_1 {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
