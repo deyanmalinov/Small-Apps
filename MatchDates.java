cd8a0a72da8704dbcd1db5f0c31164de84c729cb
DPM <deyanm@gmail.com>
2024-08-19 02:20:55 +0300

    Update

--- MatchDates.java
+++ MatchDates.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class MatchDates {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             String string = scan.nextLine();
