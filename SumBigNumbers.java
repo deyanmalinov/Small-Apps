75151d8412305c9e7de7e97d598c1100cc6d3ba0
DPM <deyanm@gmail.com>
2024-08-21 06:30:52 +0300

    Update

--- SumBigNumbers.java
+++ SumBigNumbers.java
@@ -1,11 +1,13 @@
 package com.DPM;
 import java.math.BigInteger;
 import java.util.*;
-
 public class SumBigNumbers {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         BigInteger fnum = scan.nextBigInteger();
+
         BigInteger snum = scan.nextBigInteger();
 
         System.out.println(fnum.add(snum));
