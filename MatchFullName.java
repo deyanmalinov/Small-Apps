6fef34b339c84fcd247df0936454aca2a1caff08
DPM <deyanm@gmail.com>
2024-08-22 02:21:15 +0300

    Update

--- MatchFullName.java
+++ MatchFullName.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class MatchFullName {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
