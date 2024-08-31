6e8bc0cfba26d1f9e44afdc9e94b3581edd50e84
DPM <deyanm@gmail.com>
2024-08-31 06:32:18 +0300

    Update

--- SumOfChars.java
+++ SumOfChars.java
@@ -1,24 +1,20 @@
 package com.DPM;
-
 import java.util.*;
-
 public class SumOfChars {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-            int lines = Integer.parseInt(scan.nextLine());
+        int lines = Integer.parseInt(scan.nextLine());
 
-            int sum = 0;
+        int sum = 0;
 
-            for (int i = 0; i < lines; i++) {
-                String chark = scan.nextLine();
-                char znak = chark.charAt(0);
-                int znakASCI = znak;
-                sum +=znakASCI;
-
-            }
-            System.out.println("The sum equals: " + sum);
+        for (int i = 0; i < lines; i++) {
+            String chark = scan.nextLine();
+            char znak = chark.charAt(0);
+            int znakASCI = znak;
+            sum += znakASCI;
         }
-
+        System.out.println("The sum equals: " + sum);
+    }
 }
