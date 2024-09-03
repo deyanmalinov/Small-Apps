da1ccaa977fbea295b9b466b02bf462d81adbbcb
DPM <deyanm@gmail.com>
2024-09-03 06:29:09 +0300

    Update

--- SubAndAddDots.java
+++ SubAndAddDots.java
@@ -1,8 +1,9 @@
 package com.DPM;
-
 public class SubAndAddDots {
     public static void main(String[] args) {
+
         String word = "text";
+
         System.out.println(word.substring(0,2)+ "...");
     }
 }
