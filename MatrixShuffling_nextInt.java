84022548452087acd5ed510ecdd1d5e3ebe44bf7
DPM <deyanm@gmail.com>
2024-09-06 02:23:14 +0300

    Update

--- MatrixShuffling_nextInt.java
+++ MatrixShuffling_nextInt.java
@@ -1,6 +1,6 @@
 package com.DPM;
 import java.util.Scanner;
-public class Main {
+public class MatrixShuffling_nextInt {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 //        String[] line = scan.nextLine().split(" ");
