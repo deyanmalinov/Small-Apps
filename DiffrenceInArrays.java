8c2d40444ce2782363442a962a21807d728f8a58
DPM <deyanm@gmail.com>
2024-09-08 05:52:47 +0300

    Update

--- DiffrenceInArrays.java
+++ DiffrenceInArrays.java
@@ -1,17 +1,19 @@
 package com.DPM;
-
 public class DiffrenceInArrays {
     public static void main(String[] args) {
+
         int[] array = {10, 5, 3, 8, -15};
+
         int difference = findDifference(array);
+
         System.out.println("Difference " + difference);
     }
 
     public static int findDifference(int[] array) {
 
         int min = array[0];
-        int max = array[0];
 
+        int max = array[0];
 
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
