afe9cf6cbeea34bcd273f903c5e664a87f084167
DPM <deyanm@gmail.com>
2024-09-09 06:29:21 +0300

    Update

--- SubString.java
+++ SubString.java
@@ -1,11 +1,12 @@
 package com.DPM;
-
 import java.util.*;
-
 public class SubString {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String sub = scan.nextLine();
+
         String line = scan.nextLine();
 
         while (line.contains(sub)){
