84ca76dc24a7a3fb58108618f4e3b0ed67d207f8
DPM <deyanm@gmail.com>
2024-09-17 05:53:48 +0300

    Update

--- Elevator.java
+++ Elevator.java
@@ -1,19 +1,21 @@
 package com.DPM;
-
 import java.util.*;
-
 public class Elevator {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             int numPeople = Integer.parseInt(scan.nextLine());
+
             int capasiti = Integer.parseInt(scan.nextLine());
 
             int cours = numPeople / capasiti;
+
             int cous = numPeople % capasiti;
+
             if (cous != 0){
                 cours++;
             }
             System.out.println(cours);
     }
-
 }
