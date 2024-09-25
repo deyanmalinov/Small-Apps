e131295e5fcd5cd340e95dbb81a335b0a0ec1012
DPM <deyanm@gmail.com>
2024-09-25 06:00:53 +0300

    Update

--- HotPotato.java
+++ HotPotato.java
@@ -1,25 +1,24 @@
 package com.DPM;
-
 import java.util.ArrayDeque;
 import java.util.Scanner;
-
 public class HotPotato {
     public static void main(String[] args){
+
         Scanner scan = new Scanner(System.in);
+
         ArrayDeque<String> queue = new ArrayDeque<>();
+
         String line ;
 
-        while (!"print".equalsIgnoreCase(line= scan.nextLine())){
+        while (!"Print".equalsIgnoreCase(line= scan.nextLine())){
 
-            if (!line.equals("cancel")){
+            if (!line.equals("Cancel")){
                 queue.offer(line);
-
-
             }else {
                 if (queue.size() !=0){
                     System.out.println("Canceled " + queue.poll());
                 }else {
-                    System.out.println("Printer is on standby");
+                    System.out.println("Printer is on Standby");
                 }
             }
         }
