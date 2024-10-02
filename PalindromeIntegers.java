3a601e818901a066f5d0f7dbfeb97ad7e11bccd8
DPM <deyanm@gmail.com>
2024-10-03 02:29:24 +0300

    Update

--- PalindromeIntegers.java
+++ PalindromeIntegers.java
@@ -1,6 +1,6 @@
 package com.DPM;
 import java.util.*;
-public class Main {
+public class PalindromeIntegers {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             String line = "";
