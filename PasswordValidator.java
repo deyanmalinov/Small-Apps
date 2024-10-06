c40d5c00ac96af522dab3dafa8709842f8900d37
DPM <deyanm@gmail.com>
2024-10-07 02:31:34 +0300

    Update

--- PasswordValidator.java
+++ PasswordValidator.java
@@ -2,7 +2,7 @@ package com.DPM;
 
 import java.util.*;
 
-public class Main {
+public class PasswordValidator {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             String pass = scan.nextLine();
