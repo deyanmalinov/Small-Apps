e054ccea6d6dd2b51c657c9932501a254cb18e47
DPM <deyanm@gmail.com>
2024-10-10 05:56:27 +0300

    Update

--- FactorialDivision.java
+++ FactorialDivision.java
@@ -1,24 +1,23 @@
 package com.DPM;
-
 import java.util.*;
-
 public class FactorialDivision {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             double numOne = Double.parseDouble(scan.nextLine());
+
             double secNum = Double.parseDouble(scan.nextLine());
 
             double devisRes = factor(numOne) / factor(secNum);
-            System.out.printf("%.2f", devisRes);
 
-    }
+            System.out.printf("%.2f", devisRes);
+        }
     public static double factor (double num){
             double fac = 1;
         for (int i = 2; i <= num; i++) {
             fac *= i;
-
         }
         return fac;
     }
-
 }
