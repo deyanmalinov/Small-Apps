788e76ae710a989470ac67f6248d821cc87a714d
DPM <deyanm@gmail.com>
2024-10-13 06:34:32 +0300

    Update

--- TriplesOfLatinLetters.java
+++ TriplesOfLatinLetters.java
@@ -1,11 +1,12 @@
 package com.DPM;
-
 import java.util.*;
-
 public class TriplesOfLatinLetters {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             int n = Integer.parseInt(scan.nextLine());
+
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     for (int k = 0; k < n ; k++) {
