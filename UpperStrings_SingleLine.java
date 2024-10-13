97ffdcfdf03823c0100f219137165c3aa7f90bb1
DPM <deyanm@gmail.com>
2024-10-13 06:34:41 +0300

    Update

--- UpperStrings_SingleLine.java
+++ UpperStrings_SingleLine.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
 public class UpperStrings_SingleLine {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
 
         List<String> names = Arrays.stream(scan.nextLine()
@@ -21,7 +21,5 @@ Arrays.stream(scan.nextLine()
                 .collect(Collectors.toList()).stream()
                 .map(name -> name.toUpperCase())
                 .forEach(name -> System.out.print(name + " "));
-
-
     }
 }
