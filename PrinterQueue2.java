a5801f2a0f5e0785ce9ac850d2e5430ded938d3e
DPM <deyanm@gmail.com>
2024-10-17 02:33:06 +0300

    Update

--- PrinterQueue2.java
+++ PrinterQueue2.java
@@ -2,7 +2,7 @@ package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Main {
+public class PrinterQueue2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayDeque<String> queue = new ArrayDeque<>();
