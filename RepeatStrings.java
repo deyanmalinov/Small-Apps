911ade1b2fae1e7911430d5bb1f368edd7318b06
DPM <deyanm@gmail.com>
2024-10-29 02:35:40 +0200

    Update

--- RepeatStrings.java
+++ RepeatStrings.java
@@ -2,7 +2,7 @@ package com.DPM;
 
 import java.util.*;
 
-public class Main {
+public class RepeatStrings {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String [] line = scan.nextLine().split(" ");
