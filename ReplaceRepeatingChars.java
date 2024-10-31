628ecb99d7dd6dcf0bb292a2309168e5b5ea0e4c
DPM <deyanm@gmail.com>
2024-10-31 02:36:03 +0200

    Update

--- ReplaceRepeatingChars.java
+++ ReplaceRepeatingChars.java
@@ -2,7 +2,7 @@ package com.DPM;
 
 import java.util.*;
 
-public class Main {
+public class ReplaceRepeatingChars {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String text = scan.nextLine();
