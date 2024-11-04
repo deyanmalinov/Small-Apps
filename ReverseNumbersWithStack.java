4855cbe4aeae7c2f259f67f6c1234adc5bb000c1
DPM <deyanm@gmail.com>
2024-11-04 02:37:26 +0200

    Update

--- ReverseNumbersWithStack.java
+++ ReverseNumbersWithStack.java
@@ -2,7 +2,7 @@ package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Main {
+public class ReverseNumbersWithStack {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String [] line = scan.nextLine().split(" ");
