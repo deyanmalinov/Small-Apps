84bb5f5ffe8f29af9e0fe748bf2329a00ee330d7
DPM <deyanm@gmail.com>
2024-11-08 05:57:44 +0200

    Update

--- FindEvensOrOdds.java
+++ FindEvensOrOdds.java
@@ -2,17 +2,25 @@ package com.DPM;
 import java.util.Scanner;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
-
 public class FindEvensOrOdds {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split(" ");
+
         String evenOrOdd = scan.nextLine();
+
         Predicate<Integer> even = x -> x % 2 == 0;
+
         Predicate<Integer> odd = x -> x % 2 != 0;
+
         int start = Integer.parseInt(line[0]);
+
         int end = Integer.parseInt(line[1]);
+
         Predicate<Integer> type = even;
+
         if (evenOrOdd.equals("odd")){
             type = odd;
         }
