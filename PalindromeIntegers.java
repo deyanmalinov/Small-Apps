9db85d2430aaa8952415478e71aff7d1b928b469
DPM <deyanm@gmail.com>
2024-11-09 03:52:45 +0200

    Update

--- PalindromeIntegers.java
+++ PalindromeIntegers.java
@@ -8,20 +8,20 @@ public class PalindromeIntegers {
             String line = "";
 
             while (true){
-                line = scan.nextLine();
+                    line = scan.nextLine();
                 if (line.equals("END")){
                     break;
                 }
-                for (int i = 0; i <= line.length()/2; i++) {
-                    int lenth = line.length();
-                    if (line.charAt(i) == line.charAt(lenth-1)){
-                        lenth--;
-                        System.out.println(true);
-                        break;
-                    }else {
-                        System.out.println(false);
-                        break;
-                    }
+            for (int i = 0; i <= line.length()/2; i++) {
+                int lenth = line.length();
+                if (line.charAt(i) == line.charAt(lenth-1)){
+                    lenth--;
+                    System.out.println(true);
+                    break;
+                }else{
+                    System.out.println(false);
+                    break;
+                }
             }
         }
     }
