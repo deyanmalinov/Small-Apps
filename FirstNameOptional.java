23852a188a0edf95f534408badbaecf144aab178
DPM <deyanm@gmail.com>
2024-11-17 05:58:14 +0200

    Update

--- FirstNameOptional.java
+++ FirstNameOptional.java
@@ -3,18 +3,23 @@ import java.util.*;
 import java.util.stream.Stream;
 public class FirstNameOptional {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         List<String> names = Arrays.asList(scan.nextLine().split(" "));
+
         HashSet<Character> letters = new HashSet<>();
 
         Stream.of(scan.nextLine()
                 .split(" "))
                 .map(ch -> ch.toLowerCase().charAt(0))
                 .forEach(ch -> letters.add(ch));
+
         Optional<String> name = names.stream()
                 .filter(str -> letters.contains(str.toLowerCase().charAt(0)))
                 .sorted()
                 .findFirst();
+
         if (name.isPresent()){
             System.out.println(name.get());
         }else {
