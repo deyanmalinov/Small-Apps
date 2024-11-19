9fa07bb3084a8aaad792fc3ec18d90774db39a97
DPM <deyanm@gmail.com>
2024-11-19 02:41:09 +0200

    Update

--- SoftUniParty.java
+++ SoftUniParty.java
@@ -2,7 +2,7 @@ package com.DPM;
 import java.util.Scanner;
 import java.util.TreeSet;
 
-public class Main {
+public class SoftUniParty {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         TreeSet<String> guest = new TreeSet<>();
