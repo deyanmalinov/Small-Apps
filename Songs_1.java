12464466c8bac2098f755b663085f9dafb957220
DPM <deyanm@gmail.com>
2024-11-22 03:56:34 +0200

    Update

--- Songs_1.java
+++ Songs_1.java
@@ -1,13 +1,13 @@
 package com.DPM;
 import java.util.*;
-public class Songs1 {
+public class Songs_1 {
         public static void main(String[] args) {
 
             Scanner scan = new Scanner(System.in);
 
             int lines = Integer.parseInt(scan.nextLine());
 
-            List<Songs2> songs = new ArrayList<>();
+            List<Songs_2> songs = new ArrayList<>();
 
             for (int i = 0; i < lines; i++) {
                 String [] line = scan.nextLine().split("_");
@@ -15,7 +15,7 @@ public class Songs1 {
                 type = line[0];
                 name = line[1];
                 time = line[2];
-                Songs2 song = new Songs2();
+                Songs_2 song = new Songs_2();
                 song.setTypeList(type);
                 song.setName(name);
                 song.setTime(time);
@@ -23,11 +23,11 @@ public class Songs1 {
             }
             String typeList = scan.nextLine();
             if (typeList.equals("all")){
-                for (Songs2 song : songs) {
+                for (Songs_2 song : songs) {
                     System.out.println(song.getName());
                 }
             }else {
-                for (Songs2 song : songs) {
+                for (Songs_2 song : songs) {
                     if (song.getTypeList().equals(typeList)){
                         System.out.println(song.getName());
                     }
