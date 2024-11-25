c7b228d5a8ca55553304eaabda6d38532be1a36b
DPM <deyanm@gmail.com>
2024-11-25 02:42:49 +0200

    Update

--- SortByNameAndAge.java
+++ SortByNameAndAge.java
@@ -2,18 +2,18 @@ package com.DPM;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class Main {
+public class SortByNameAndAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int lines = Integer.parseInt(scan.nextLine());
-        List<Person> people = new ArrayList<>();
-        Person man = null;
+        List<Persons> people = new ArrayList<>();
+        Persons man = null;
         for (int i = 0; i < lines; i++) {
             String[] line = scan.nextLine().split(" ");
             String fname = line[0];
             String sname = line[1];
             int age = Integer.parseInt(line[2]);
-            man = new Person(fname, sname, age);
+            man = new Persons(fname, sname, age);
             people.add(man);
         }
         people.sort((fperson, sperson) -> {
@@ -24,17 +24,17 @@ public class Main {
                 return Integer.compare(fperson.getAge(), sperson.getAge());
             }
         });
-        for (Person person : people) {
+        for (Persons person : people) {
             System.out.println(person.toString());
         }
     }
 }
 
-public class Person {
+class Persons {
     private String fname;
     private String sname;
     private int age;
-    public Person(String fname, String sname, int age){
+    public Persons(String fname, String sname, int age){
         this.fname=fname;
         this.sname=sname;
         this.age=age;
