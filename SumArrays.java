59850426c26f7f16f6674c8a5dc7f622e517fb6d
DPM <deyanm@gmail.com>
2024-12-14 02:48:06 +0200

    Update

--- SumArrays.java
+++ SumArrays.java
@@ -3,7 +3,7 @@ package com.DPM;
 public class SumArrays {
     public static void main(String[] args) {
         int sum = 0;
-        int[] numbers = {1, 2, 3, 4, 5, 6};
+        int[] numbers = {1, 2, 3, 4, 5};
         if(numbers.length>0){
             for (int i = 0; i < numbers.length; i++) {
                 sum = sum + numbers[i];
