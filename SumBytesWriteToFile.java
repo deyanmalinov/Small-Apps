447aa1cc30bb8bed2bd93a708a64f277a94b0328
DPM <deyanm@gmail.com>
2024-12-17 02:48:52 +0200

    Update

--- SumBytesWriteToFile.java
+++ SumBytesWriteToFile.java
@@ -1,6 +1,6 @@
 package com.DPM;
 import java.io.*;
-public class Main {
+public class SumBytesWriteToFile {
     public static void main(String[] args) throws IOException {
         String path = "D:\\Coding\\BG_Softuni\\Java Advanced - January 2019" +
                 "\\04.1. Java-Advanced-Files-and-Directories-Exercises\\" +
