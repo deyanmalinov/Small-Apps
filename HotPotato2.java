ef896a5353021ecce17667002456a4b151a1bd1c
DPM <deyanm@gmail.com>
2024-12-20 06:01:15 +0200

    Update

--- HotPotato2.java
+++ HotPotato2.java
@@ -1,16 +1,21 @@
 package com.DPM;
 import java.util.ArrayDeque;
 import java.util.Scanner;
-
 public class HotPotato2 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         String[] line = scan.nextLine().split("\\s+");
+
         int num = Integer.parseInt(scan.nextLine());
+
         ArrayDeque<String> queue = new ArrayDeque<>();
+
         for (int i = 0; i < line.length; i++) {
             queue.offer(line[i]);
         }
+
         while (queue.size() > 1){
                 for (int i = 0; i < num-1; i++) {
                     String name = queue.poll();
