aab4c2115a9867ff2cd7c63522946a64dc347edd
DPM <deyanm@gmail.com>
2024-12-29 05:26:39 +0200

    Update

---
+++ Split_by_Space.java
@@ -0,0 +1,28 @@
+package com.DPM;
+
+import java.util.Scanner;
+
+public class Split_by_Space {
+        public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+
+            String nums = scan.nextLine();
+
+            String [] mas = nums.split(" ");
+
+            int [] chisla = new int [mas.length];
+
+            int sum =0;
+
+            for (int i = 0; i < chisla.length; i++) {
+                chisla[i] = Integer.parseInt(mas[i]);
+                if (chisla[i] %2 == 0){
+                    sum += chisla[i];
+                }
+
+            }
+            System.out.println(sum);
+        }
+
+}
