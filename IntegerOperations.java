3cb582c9399c2bcff32d2deb9a08212887c36db0
DPM <deyanm@gmail.com>
2024-12-31 06:01:50 +0200

    Update

--- IntegerOperations.java
+++ IntegerOperations.java
@@ -1,19 +1,24 @@
 package com.DPM;
-
 import java.util.*;
-
 public class IntegerOperations {
         public static void main(String[] args) {
+
             Scanner scan = new Scanner(System.in);
+
             long fnum = Long.parseLong(scan.nextLine());
+
             long snum = Long.parseLong(scan.nextLine());
+
             long tnum = Long.parseLong(scan.nextLine());
+
             long cnum = Long.parseLong(scan.nextLine());
 
             long sum = fnum + snum;
+
             long dev = sum / tnum;
+
             long sumF = dev*cnum;
+
             System.out.println(sumF);
     }
-
 }
